bdce14bda355f30847bb5737a5fa59fc3fb6a6d0
Omar Karame <omarkarmae21@gmail.com>
2022-09-27 16:59:01 +0100

    Added pseudocode to the files in searchbar comp

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,13 @@
+import './Searchbar.scss';
+
+const Searchbar = () => {
+  return (
+    <div className='search-bar'>
+        {/* magnifying glass icon to the left of input */}
+        {/* input containing a placeholder that says Search for articles... */}
+        {/* create an onChange that is non functional that takes in the function as a prop */}
+    </div>
+  )
+}
+
+export default Searchbar
