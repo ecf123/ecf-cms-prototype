1076307fa7407116b0378ba777ce2c79ca3c9ff8
Chen <liangchen_amelia@163.com>
2022-09-27 15:26:50 +0100

    setting out components

---
+++ src/components/EmailBox/EmailBox.jsx
@@ -0,0 +1,1 @@
+import "./EmailBox.scss";
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,13 @@
+import "./Title.scss";
+
+const Title = () => {
+
+    return (
+
+        <div>
+            <h1>Welcome Back</h1>
+        </div>
+        
+        
+    )
+}
