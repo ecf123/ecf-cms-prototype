1a294ec38a418789030171887abb3bbe8ab94a3f
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-09-28 16:21:14 +0100

    added variables to styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,8 @@
-import HeaderProfile from "../../components/HeaderProfile/HeaderProfile";
 import Layout from "../../components/Layout/Layout";
 
 const Home = () => {
   return (
     <Layout>
-      <HeaderProfile />
       <h1>Page Heading</h1>
       <h2>Section Heading</h2>
       <h3>Panel Heading</h3>
