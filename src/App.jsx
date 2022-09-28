415910dd26af00c06ca94a5f0649f222310c3077
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-09-28 15:12:34 +0100

    Fixed errors in App.jsx - revert to unchanged

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
 import "./components/ArticleListCards/ArticleListCards";
-import ArticleListCards from "./components/ArticleListCards/ArticleListCards";
 
 const App = () => {
   return (
