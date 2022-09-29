25df1fe0dac110033e374afbe4901a2f1c017bd3
Amelia Baldwin <amelia01000010@gmail.com>
2022-09-29 15:54:48 +0100

    svgs added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react'
 import Logo from '../Logo/Logo';
 import NavBarItem from '../NavBarItem/NavBarItem';
 import './NavBar.scss';
-
+import cog from '../../assets/cog-icon.svg'
+import article from '../../assets/article-book-icon.svg';
+import dashboard from '../../assets/dashboard-windows-sqare.svg';
+import pathways from '../../assets/pathways-chain-icon.svg';
+import marketplace from '../../assets/marketplace-shopping-trolley-icon.svg';
+import users from '../../assets/users-people-icon.svg';
 
  const NavBar = () => {
   const [ selected, setSelected ] = useState("Dashboard");
@@ -18,14 +23,14 @@ import './NavBar.scss';
         </div>
         
         <ul className='navBar__linkList'>
-          <li className='navBar__listItem'><NavBarItem imageSrc="" text="Dashboard" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
-          <li className='navBar__listItem'><NavBarItem imageSrc="" text="Pathways" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
-          <li className='navBar__listItem'><NavBarItem imageSrc="" text="Articles" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
-          <li className='navBar__listItem'><NavBarItem imageSrc="" text="Marketplace" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
-          <li className='navBar__listItem'><NavBarItem imageSrc="" text="Users" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
+          <li className='navBar__listItem'><NavBarItem imageSrc={dashboard} text="Dashboard" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
+          <li className='navBar__listItem'><NavBarItem imageSrc={pathways} text="Pathways" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
+          <li className='navBar__listItem'><NavBarItem imageSrc={article} text="Articles" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
+          <li className='navBar__listItem'><NavBarItem imageSrc={marketplace} text="Marketplace" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
+          <li className='navBar__listItem'><NavBarItem imageSrc={users} text="Users" selected={selected} getCurrentPage={getCurrentPage} link="" /></li>
         </ul>
 
-        <NavBarItem imageSrc="" text="Settings" selected={selected} getCurrentPage={getCurrentPage} link="" />
+        <NavBarItem imageSrc={cog} text="Settings" selected={selected} getCurrentPage={getCurrentPage} link="" />
       </div>
     </div>
   )
