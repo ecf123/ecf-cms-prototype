f982cdd3c9f7956fa81e0913ef6e4a0165312e25
Gar-Yin Lam <garyin.lam@gmail.com>
2022-09-29 15:58:45 +0100

    added logo svg

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "./Logo.scss"
+import logo from '../../assets/ECFP-company-logo.svg';
 
 const Logo = ({getCurrentPage}) => {
 
@@ -10,7 +11,7 @@ const Logo = ({getCurrentPage}) => {
   return (
     <Link to="*" className='link'>
       <div className='logo' onClick={handleClick}>
-        <img src="" alt="The Future Hub Logo" className='logo__image'/>
+        <img src={logo} alt="The Future Hub Logo" className='logo__image'/>
         <p className='logo__topTitle'>THE</p>
         <h3 className='logo__title'>FUTURE HUB</h3>
       </div>
