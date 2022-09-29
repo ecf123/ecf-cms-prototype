d8ecf396a5d702a9c77f069d462c0152f67dc021
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-09-29 16:10:05 +0100

    Made changes to styles using new variables

--- src/components/ArticleListCards/ArticleListCards.jsx
+++ src/components/ArticleListCards/ArticleListCards.jsx
@@ -18,7 +18,7 @@ const ArticleListCards = () => {
         <p className='card__info'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores odit ratione fugiat esse repudiandae maxime optio</p>
         <ul className='card__links'>
             <li className='card__link card__link--purple'>Professional Development</li>
-            <li className='card__link card__link--darkblue'>Career</li>
+            <li className='card__link card__link--darkpurple'>Career</li>
         </ul>
     </div>
   )
