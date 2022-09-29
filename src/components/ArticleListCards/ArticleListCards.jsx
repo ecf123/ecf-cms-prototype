c0951c6d9d2fd7aa840975b19e0df7c2081018a1
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-09-29 17:06:32 +0100

    Removed placeholder content to replace with prop

--- src/components/ArticleListCards/ArticleListCards.jsx
+++ src/components/ArticleListCards/ArticleListCards.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import "./ArticleListCards.scss";
-import iconArrow from '../../assets/Iconarrow.png';
+import iconArrow from '../../assets/arrow-upper-right-black.svg';
 import "../../styles/_reset.scss";
 
-
-//added placeholder content for the information on the card and image. 
-//needs to have props passed down in future for the content to render each article.
-const ArticleListCards = () => {
+const ArticleListCards = ({img, date, title, articleInfo, articleLinkOne, articleLinkTwo}) => {
   return (
     <div className='card'>
-        <img className='card__image' src="https://images.unsplash.com/photo-1487528278747-ba99ed528ebc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9yJTIwaGlyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="hire" />
-        <p className='card__date'>27/09/2022</p>
-        <div className='card__container'>
-          <h2 className='card__title'>Want a winning CV? Try these 5 tips</h2>
+        <img className='card__image' src={img} alt="Article Image" />
+        <p className='card__date'>{date}</p>
+        <div className='card__header'>
+          <h2 className='card__title'>{title}</h2>
           <img className='card__icon' src={iconArrow} alt="Arrow Icon" />
         </div>
-        <p className='card__info'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores odit ratione fugiat esse repudiandae maxime optio</p>
+        <p className='card__info'>{articleInfo}</p>
         <ul className='card__links'>
-            <li className='card__link card__link--purple'>Professional Development</li>
-            <li className='card__link card__link--darkpurple'>Career</li>
+            <li className='card__link card__link--purple'>{articleLinkOne}</li>
+            <li className='card__link card__link--darkpurple'>{articleLinkTwo}</li>
         </ul>
     </div>
   )
--- src/components/ArticleListCards/ArticleListCards.test.jsx
+++ src/components/ArticleListCards/ArticleListCards.test.jsx
@@ -1,61 +1,70 @@
-import { render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ArticleListCards from "./ArticleListCards";
-import ""
 
-it ( "should render the card image", () => {
-    //Arrange
-    render(<ArticleListCards />);
-    //Act
-    const image = screen.getByAltText("hire");
-    //Assert
-    expect(image).toBeInTheDocument();
-})
+it("should render the card image", () => {
+  //Arrange
+  render(<ArticleListCards />);
+  //Act
+  const image = screen.getByAltText("Article Image");
+  //Assert
+  expect(image).toBeInTheDocument();
+});
 
-it ("should display the date of the card on the screen", () => {
-    //Arrange
-    render(<ArticleListCards />);
-    //Act
-    const date = screen.queryByText("27/09/2022");
-    //Assert
-    expect(date).toBeInTheDocument();
-})
+it("should display the date of the card on the screen", () => {
+  //Arrange
+  render(<ArticleListCards date="22/01/2022" />);
+  //Act
+  const date = screen.queryByText("22/01/2022");
+  //Assert
+  expect(date).toBeInTheDocument();
+});
 
-it ("should display the header of the card on the screen", () => {
-    //Arrange
-    render(<ArticleListCards />);
-    //Act
-    const header = screen.queryByText("Want a winning CV? Try these 5 tips");
-    //Assert
-    expect(header).toBeInTheDocument();
-})
+it("should display the header of the card on the screen", () => {
+  //Arrange
+  render(<ArticleListCards title="Test Title" />);
+  //Act
+  const header = screen.queryByText("Test Title");
+  //Assert
+  expect(header).toBeInTheDocument();
+});
 
-it ("should display the links on the screen", () => {
-    //Arrange
-    render(<ArticleListCards />);
-    //Act
-    const link1 = screen.getByText("Professional Development");
-    const link2 = screen.getByText("Career");
-    //Assert
-    expect(link1).toBeInTheDocument();
-    expect(link2).toBeInTheDocument();
-})
+it("should display the links on the screen", () => {
+  //Arrange
+  render(
+    <ArticleListCards
+      articleLinkOne="Test Link One"
+      articleLinkTwo="Test Link Two"
+    />
+  );
+  //Act
+  const link1 = screen.getByText("Test Link One");
+  const link2 = screen.getByText("Test Link Two");
+  //Assert
+  expect(link1).toBeInTheDocument();
+  expect(link2).toBeInTheDocument();
+});
 
+it("should display the article paragraph on the screen", () => {
+  //Arrange
+  render(<ArticleListCards articleInfo="Test article" />);
+  //Act
+  const paragraph = screen.getByText("Test article");
+  //Assert
+  expect(paragraph).toBeInTheDocument();
+});
 
-it ("should display the article paragraph on the screen", () => {
-    //Arrange
-    render(<ArticleListCards />);
-    //Act
-    const paragraph = screen.getByText("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores odit ratione fugiat esse repudiandae maxime optio");
-    //Assert
-    expect(paragraph).toBeInTheDocument();
-})
+it("should render the arrow icon", () => {
+  //Arrange
+  render(<ArticleListCards />);
+  //Act
+  const image = screen.getByAltText("Arrow Icon");
+  //Assert
+  expect(image).toBeInTheDocument();
+});
 
-it ( "should render the arrow icon", () => {
-    //Arrange
-    render(<ArticleListCards />);
-    //Act
-    const image = screen.getByAltText("Arrow Icon");
-    //Assert
-    expect(image).toBeInTheDocument();
-})
-
+it("Should render layout with children", () => {
+  const { card } = customRender(
+    <ArticleListCards articleInfo="Test Article" />
+  );
+  expect(card).toMatchSnapshot();
+});
