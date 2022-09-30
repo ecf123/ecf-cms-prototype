9da5c32eadfc09de2f8d3e76f3241465fd9f3eee
Omar Karame <omarkarmae21@gmail.com>
2022-09-30 16:40:37 +0100

    Added some styling to delete prompt without "btns"

--- src/components/DeletePrompt/DeletePrompt.jsx
+++ src/components/DeletePrompt/DeletePrompt.jsx
@@ -4,10 +4,14 @@ import warning from "../../assets/exclamation-mark-icon.svg"
 const DeletePrompt = ({title}) => {
   return (
     <div className="delete-prompt">
-        <img src={warning} alt="Warning Image" className="delete-prompt__image"/>
+      <div className="delete-prompt__image-conatiner">
+      <img src={warning} alt="Warning Image" className="delete-prompt__image"/>
+      </div>
         <h2 className="delete-prompt__title">Delete {title}</h2>
         <p className="delete-prompt__content">Are you sure you want to delete this {title}? This action cannot be undone.</p>
-        <button>Cancel</button><button>Delete</button>
+        <div className="delete-prompt__button-holder">
+          <button>Cancel</button><button>Delete</button>
+        </div>
     </div>
   )
 }
