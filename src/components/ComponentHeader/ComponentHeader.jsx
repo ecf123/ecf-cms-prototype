03076f24b1920e19b8c5cd72e08a3cac884fb2c1
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-09-30 15:53:19 +0100

    created branch and set up file structure

---
+++ src/components/ComponentHeader/ComponentHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../assets/"
+
+const ComponentHeader = () => {
+  return (
+    <div>
+        <h1>Pathways</h1>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default ComponentHeader
