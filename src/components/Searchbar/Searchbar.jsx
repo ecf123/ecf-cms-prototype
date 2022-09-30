ea6f2684964872e1d2c36760f91f69580ea5e6d8
Omar Karame <omarkarmae21@gmail.com>
2022-09-30 16:44:30 +0100

    Added full-stop in comment

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -6,7 +6,7 @@ const Searchbar = ({placeholderText, searchWord, handleSearchInput }) => {
   
   return (
     <div className="search-bar">
-      {/*Props being used to change the placeholder text, as well as store search word and handle the logic of the search bar*/}
+      {/*Props being used to change the placeholder text, as well as store search word and handle the logic of the search bar.*/}
       <img src={magnifyGlass} alt="Search Icon" className="search-bar__image"/>
       <input type="text" placeholder={placeholderText} value={searchWord} onChange={handleSearchInput} className="search-bar__input" data-testid="search-input"/>
     </div>
