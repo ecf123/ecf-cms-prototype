ce670ed47d88ac02e8a1b4e55f50678b6a526987
atif298 <atifuddin03@gmail.com>
2022-09-30 17:00:40 +0100

    Snapshot render tests added

--- src/components/Short/Short.test.jsx
+++ src/components/Short/Short.test.jsx
@@ -1,6 +1,17 @@
 // Short.test.jsx
 import { render, screen } from "@testing-library/react";
 import Short from "./Short.jsx";
+import {customRender} from "../../utils/testUtils";
+
+it("Should match the snapshot, without the props", () => {
+  const {container} = customRender(<Short/>);
+  expect(container).toMatchSnapshot();
+});
+
+it("Should match the snapshot, with the props", () => {
+  const {container} = customRender(<Short shortlabelText={"label"} shortplaceHolderText={"placeholder"}/>);
+  expect(container).toMatchSnapshot();
+});
 
 it("Should render the Short textarea", () => {
   // 1. Arrange
