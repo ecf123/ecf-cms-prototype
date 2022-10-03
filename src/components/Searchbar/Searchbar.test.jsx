b40ebd39e3d24e28812ce8d18fae4b8c1f7a533f
Omar Karame <omarkarmae21@gmail.com>
2022-10-03 15:04:54 +0100

    fixed typo in text

--- src/components/Searchbar/Searchbar.test.jsx
+++ src/components/Searchbar/Searchbar.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import SearchBar from "./SearchBar";
+import SearchBar from "./SearchBar.jsx";
 
 import { customRender } from "../../utils/testUtils";
 
