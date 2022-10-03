c91b1e4907a5edd9d28a3a462fcbc121b4a67830
BrookVerse <brookewaldorf90@gmail.com>
2022-10-03 15:20:20 +0100

    changed scss, make  prompt a modal and added props

--- src/components/DeletePrompt/DeletePrompt.jsx
+++ src/components/DeletePrompt/DeletePrompt.jsx
@@ -1,7 +1,7 @@
 import "./DeletePrompt.scss"
 import warning from "../../assets/exclamation-mark-icon.svg"
 
-const DeletePrompt = ({title}) => {
+const DeletePrompt = ({ title, handleCancel, handleDelete }) => {
   return (
     <div className="delete-prompt">
       <div className="delete-prompt__image-conatiner">
@@ -10,7 +10,7 @@ const DeletePrompt = ({title}) => {
         <h2 className="delete-prompt__title">Delete {title}</h2>
         <p className="delete-prompt__content">Are you sure you want to delete this {title}? This action cannot be undone.</p>
         <div className="delete-prompt__button-holder">
-          <button>Cancel</button><button>Delete</button>
+          <button onClick={handleCancel}>Cancel</button><button onClick={handleDelete}>Delete</button>
         </div>
     </div>
   )
