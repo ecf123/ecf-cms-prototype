68dae5b51f1381a1f54659d11d74c0cb9643e2c6
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-10-04 15:55:45 +0100

    changes made with props and renamed.

--- src/components/PageTitle/PageTitle.test.jsx
+++ src/components/PageTitle/PageTitle.test.jsx
@@ -1,16 +1,16 @@
 import { render, screen} from "@testing-library/react";
 import { customRender } from "../../utils/testUtils";
-import ComponentHeader from "./ComponentHeader";
+import PageTitle from "./PageTitle";
 
 it("should have a title displayed", () => {
-    render(<ComponentHeader title="test title"/>);
+    render(<PageTitle title="test title"/>);
     const title = screen.getByText("test title");
     expect(title).toBeInTheDocument();
 })
 
 it("Should render layout with children", () => {
   const { container } = customRender(
-    <ComponentHeader title="test title"/>
+    <PageTitle title="test title"/>
   );
   expect(container).toMatchSnapshot();
 });
