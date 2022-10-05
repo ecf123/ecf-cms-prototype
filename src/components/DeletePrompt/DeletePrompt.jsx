7a5fb70d3f72f6fc2550b02d002aed76ef4f898f
BrookVerse <brookewaldorf90@gmail.com>
2022-10-05 22:50:45 +0100

    used npm run format

--- src/components/DeletePrompt/DeletePrompt.jsx
+++ src/components/DeletePrompt/DeletePrompt.jsx
@@ -3,25 +3,36 @@ import warning from "../../assets/exclamation-mark-icon.svg";
 import Button from "../Button/Button";
 
 const DeletePrompt = ({ title, handleCancel, handleDelete }) => {
-  {/*This is the component for the delete prompt box, it will need props for the handling of each click
-    as well as a prop for the title of what is being deleted.*/}
+  {
+    /*This is the component for the delete prompt box, it will need props for the handling of each click
+    as well as a prop for the title of what is being deleted.*/
+  }
   return (
     <div className="delete-prompt">
       <div className="delete-prompt__image-conatiner">
-        <img src={warning} alt="Warning Image" className="delete-prompt__image" />
+        <img
+          src={warning}
+          alt="Warning Image"
+          className="delete-prompt__image"
+        />
       </div>
       <h2 className="delete-prompt__title">Delete {title}</h2>
-      <p className="delete-prompt__content">Are you sure you want to delete this {title}? This action cannot be undone.</p>
+      <p className="delete-prompt__content">
+        Are you sure you want to delete this {title}? This action cannot be
+        undone.
+      </p>
       <div className="delete-prompt__button-holder">
-        <Button 
-        style={"button white rounded-square-border border modal"} 
-        textStyle={"text modal-text white"} buttonText={"Cancel"} 
-        onClick={handleCancel}
+        <Button
+          style={"button white rounded-square-border border modal"}
+          textStyle={"text modal-text white"}
+          buttonText={"Cancel"}
+          onClick={handleCancel}
         />
-        <Button 
-        style={"button red rounded-square-border modal"} 
-        textStyle={"text modal-text red"} buttonText={"Delete"} 
-        onClick={handleDelete}
+        <Button
+          style={"button red rounded-square-border modal"}
+          textStyle={"text modal-text red"}
+          buttonText={"Delete"}
+          onClick={handleDelete}
         />
       </div>
     </div>
--- src/components/DeletePrompt/DeletePrompt.test.jsx
+++ src/components/DeletePrompt/DeletePrompt.test.jsx
@@ -1,13 +1,8 @@
 import { customRender } from "../../utils/testUtils";
 import DeletePrompt from "./DeletePrompt.jsx";
 
-it("Should render DeletePromt with children", () => {
-  const { container } = customRender(
-    <DeletePrompt>
-      <p>some content</p>
-    </DeletePrompt>
-  );
+it("Should render DeletePrompt with children", () => {
+  const { container } = customRender(<DeletePrompt title={"Title"} />);
 
   expect(container).toMatchSnapshot();
 });
-
