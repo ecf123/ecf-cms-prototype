ec9281ce5be705715be24fc2634cf04eedecbaa5
Omar Karame <omarkarmae21@gmail.com>
2022-10-06 15:50:09 +0100

    Added files and pseudo code

---
+++ src/containers/AddPathway/AddPathway.jsx
@@ -0,0 +1,27 @@
+import './AddPathway.scss';
+import Button from "../../components/Button/Button";
+
+const AddPathway = () => {
+  return (
+    <div className='add-pathway'>
+        <div className='headings'>
+            {/* Header of container */}
+            
+            {/* Description of container */}    
+        </div>
+        <div>
+            {/* Pathway Name */}
+            {/* Pathway Overview */}
+            {/* Pahway Career Description */}
+            {/* Pathway Career(s) */}
+            <div className='buttons'>
+                <Button />
+                <Button />
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default AddPathway
