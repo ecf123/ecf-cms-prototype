0ada4017fa9fd3100cc6d1c1175fc4e5b5105b63
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-10-06 14:57:44 +0100

    created login input box component

---
+++ src/components/LoginInputBox/LoginInputBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './LoginInputBox.scss'
+const LoginInputBox = ({labelText}) => {
+  return (
+    <div>
+        <label htmlFor="inputBox">{labelText}</label>
+        <input className="inputBox" type="text" />
+    </div>
+  )
+}
+export default LoginInputBox
