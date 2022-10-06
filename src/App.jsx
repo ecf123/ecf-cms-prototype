93578baca6bec4f278e87bf3aa63081d263ffdca
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-10-06 16:57:35 +0100

    corrected tests - all now passing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
-import Card from './components/Card/Card.jsx';
-import image from "./assets/for-hire-image.svg"
 
 const App = () => {
-
-  const arr = ["Developer", "Developer"]
-
   return (
-    // <Routes>
-    //   <Route path="*" element={<Home />} />
-    // </Routes>
-    <Card img = {image} dateOrTime = "12:02" title ="Test" cardInfo ="lorafffffffff fffffffffffff ffffffffffsdfgdgsdfgsdfgsdfgsdfgsdfgsdfgs dfgsdfgsdfgsdfg sdfdfgsdfssdfgfffffffem" links={arr} />
+    <Routes>
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 };
 
