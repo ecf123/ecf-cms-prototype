a621d8b84c6a672d680d50035b1592e1bf76ed4d
Amelia Baldwin <amelia01000010@gmail.com>
2022-10-06 16:58:08 +0100

    Styling added

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -1,13 +1,16 @@
 import "./DropDown.scss";
 import topicArrow from "../../assets/upside-down-arrow-grey.svg";
 
-const DropDown = ({topicTitle}) => {
+const DropDown = ({topicTitle,lessonType,lessonTitle, display}) => {
   return (
-    <div className="dropDown">
-        <div className="dropDownTopics">
-            <h3>{topicTitle}</h3>
-            <img src={topicArrow} alt="" />
-            <div className="dropDownLessons"></div>
+    <div className="drop-down">
+        <div className="drop-down__topic" onClick={display}>
+            <h3 className="drop-down__topic-title">{topicTitle}</h3>
+            <img src={topicArrow} alt={topicArrow} className="drop-down__img"/>
+        </div>
+        <div className="drop-down__lessons">
+              <img src={lessonType} alt={lessonType} className="drop-down__img"/>
+              <p>{lessonTitle}</p>
         </div>
     </div>
   )
