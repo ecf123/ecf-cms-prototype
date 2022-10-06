d9e650d55205b783d53ddaf25b2a54cf35523e34
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-06 16:22:52 +0100

    create component files

---
+++ src/components/PathwayIntroduction/PathwayIntroduction.jsx
@@ -0,0 +1,10 @@
+import "./PathwayIntroduction.scss"
+import React from 'react'
+
+const PathwayIntroduction = () => {
+  return (
+    <div>PathwayIntroduction</div>
+  )
+}
+
+export default PathwayIntroduction
