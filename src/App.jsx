96b94ffd6a9ed04261e3a68a9ad4f6c0aa1694f2
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-10-06 15:53:06 +0100

    amended links to loop through any number passed in

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home/Home";
 import "./styles/main.scss";
+import Card from './components/Card/Card.jsx';
 
 const App = () => {
+
+  const arr = [1, 2, 3, 4]
+
   return (
-    <Routes>
-      <Route path="*" element={<Home />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="*" element={<Home />} />
+    // </Routes>
+    <Card dateOrTime = "12:02" title ="Test" cardInfo ="Hello" links={arr} />
   );
 };
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,27 +3,33 @@ import "./Card.scss";
 import iconArrow from "../../assets/arrow-upper-right-black.svg";
 import "../../styles/_reset.scss";
 
-const Card = ({
-  img,
-  date,
-  title,
-  cardInfo,
-  cardLinkOne,
-  cardLinkTwo,
-}) => {
+const Card = ({ img, dateOrTime, title, cardInfo, links }) => {
+  const linksArr = links.map((link, index) => {
+    if (index % 2 == 0) {
+      return (
+        <li key={index + 1} className="card__link card__link--purple">
+          {link}
+        </li>
+      );
+    } else {
+      return (
+        <li key={index + 1} className="card__link card__link--darkpurple">
+          {link}
+        </li>
+      );
+    }
+  });
+
   return (
     <div className="card">
       <img className="card__image" src={img} alt="Card Image" />
-      <p className="card__date">{date}</p>
+      <p className="card__date">{dateOrTime}</p>
       <div className="card__header">
         <h2 className="card__title">{title}</h2>
         <img className="card__icon" src={iconArrow} alt="Arrow Icon" />
       </div>
       <p className="card__info">{cardInfo}</p>
-      <ul className="card__links">
-        <li className="card__link card__link--purple">{cardLinkOne}</li>
-        <li className="card__link card__link--darkpurple">{cardLinkTwo}</li>
-      </ul>
+      <ul className="card__links">{linksArr}</ul>
     </div>
   );
 };
