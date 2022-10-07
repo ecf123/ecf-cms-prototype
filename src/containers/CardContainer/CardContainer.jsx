c8a5b202e3f33d59e39773499f9cd570cbe70f51
AdrianRotila <adrianrotila@gmail.com>
2022-10-07 14:39:36 +0100

    created card container folder and files

---
+++ src/containers/CardContainer/CardContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../../components/Card/Card'
+import './CardContainer.scss'
+import image from "../../assets/for-hire-image.svg"
+
+const CardContainer = () => {
+
+  return (
+
+    <div><Card img = {image} 
+    dateOrTime, title, cardInfo, links/></div>
+  )
+}
+
+export default CardContainer
