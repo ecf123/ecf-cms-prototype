29a67a902adb93f7786f8a2b3c5cfc018942b43a
Amelia Baldwin <amelia01000010@gmail.com>
2022-10-07 14:55:50 +0100

    add list

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -1,17 +1,23 @@
 import "./DropDown.scss";
 import topicArrow from "../../assets/upside-down-arrow-grey.svg";
 
-const DropDown = ({topicTitle,lessonType,lessonTitle, display}) => {
+const DropDown = ({topicTitle,lessonType,lessonTitle, lessonLink}) => {
   return (
     <div className="drop-down">
-        <div className="drop-down__topic" onClick={display}>
-            <h3 className="drop-down__topic-title">{topicTitle}</h3>
+        <div className="drop-down__topic" >
+          <label htmlFor="#click"><span>{topicTitle}</span></label>
+          <input type="checkbox" id="click" />
+           {/* <h3 className="drop-down__topic-title">{topicTitle}</h3> */}
             <img src={topicArrow} alt={topicArrow} className="drop-down__img"/>
         </div>
-        <div className="drop-down__lessons">
-              <img src={lessonType} alt={lessonType} className="drop-down__img"/>
-              <p>{lessonTitle}</p>
-        </div>
+        <ul className="drop-down__list">
+              <li className="drop-down__lesson">
+                <a href={lessonLink}>
+                <img src={lessonType} alt={lessonType} className="drop-down__img"/>
+                <p>{lessonTitle}</p>
+                </a>
+              </li>
+        </ul>
     </div>
   )
 }
