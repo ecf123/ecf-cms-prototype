ceb6fcc565c8d30e25b9d503908975211365d301
Amelia Baldwin <amelia01000010@gmail.com>
2022-10-07 15:01:40 +0100

    list

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import DropDown from "../DropDown/DropDown";
 import "./Layout.scss";
 import topicArrow from "../../assets/upside-down-arrow-grey.svg";
 const Layout = ({ children }) => {
-  return <div className="layout"><DropDown topicTitle="Business"lessonType={topicArrow} lessonTitle="Suits"/>{children}</div>;
+  return <div className="layout"><DropDown topicTitle="Business"lessonType={topicArrow} lessonTitle="Suits" lessonLink="£"/>{children}</div>;
 };
 
 export default Layout;
