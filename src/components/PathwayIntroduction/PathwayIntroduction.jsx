7e0623ad69990ef91b2681789fcebbaedeaef7fd
Parham2002 <parhamm2002@gmail.com>
2022-10-07 14:32:35 +0100

    done component styling

--- src/components/PathwayIntroduction/PathwayIntroduction.jsx
+++ src/components/PathwayIntroduction/PathwayIntroduction.jsx
@@ -1,9 +1,14 @@
 import "./PathwayIntroduction.scss"
 import React from 'react'
 
-const PathwayIntroduction = () => {
+const PathwayIntroduction = (props) => {
+  const {img, description} = props;
   return (
-    <div>PathwayIntroduction</div>
+    <div className="pathway-introduction">
+      <img className="pathway-introduction__img" src={img} alt="Pathway-thumbnail" />
+      <h2 className="pathway-introduction__title">Pathway Overview</h2>
+      <p className="pathway-introduction__description">{description}</p>
+      </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import Layout from "../../components/Layout/Layout";
+import PathwayIntroduction from "../../components/PathwayIntroduction/PathwayIntroduction";
+import moneyTree from "../../assets/money-growing-on-tree.svg"
 
 const Home = () => {
   return (
@@ -12,7 +14,9 @@ const Home = () => {
         maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
         necessitatibus.
       </p>
+      <PathwayIntroduction img={moneyTree} description={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non errordolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatismaxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,necessitatibus."}/>
     </Layout>
+
   );
 };
 
