ed5e74238d8602ac931dff4f86f706661d74be0d
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-07 15:14:47 +0100

    PathwayIntroduction styling and layout changes

--- src/components/PathwayIntroduction/PathwayIntroduction.jsx
+++ src/components/PathwayIntroduction/PathwayIntroduction.jsx
@@ -1,15 +1,19 @@
-import "./PathwayIntroduction.scss"
-import React from 'react'
+import "./PathwayIntroduction.scss";
+import React from "react";
 
 const PathwayIntroduction = (props) => {
-  const {img, description} = props;
+  const { image, description } = props;
   return (
     <div className="pathway-introduction">
-      <img className="pathway-introduction__img" src={img} alt="Pathway-thumbnail" />
+      <img
+        className="pathway-introduction__image"
+        src={image}
+        alt="pathway thumbnail"
+      />
       <h2 className="pathway-introduction__title">Pathway Overview</h2>
       <p className="pathway-introduction__description">{description}</p>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default PathwayIntroduction
+export default PathwayIntroduction;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,4 @@
 import Layout from "../../components/Layout/Layout";
-import PathwayIntroduction from "../../components/PathwayIntroduction/PathwayIntroduction";
-import moneyTree from "../../assets/money-growing-on-tree.svg"
 
 const Home = () => {
   return (
@@ -14,9 +12,7 @@ const Home = () => {
         maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
         necessitatibus.
       </p>
-      <PathwayIntroduction img={moneyTree} description={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non errordolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatismaxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,necessitatibus."}/>
     </Layout>
-
   );
 };
 
