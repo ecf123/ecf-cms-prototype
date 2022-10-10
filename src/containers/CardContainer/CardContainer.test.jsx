37854f7b9b5acdcd458a2e4be750d5e312d72826
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-10-10 15:31:40 +0100

    added test to card container to check multiple cards render

--- src/containers/CardContainer/CardContainer.test.jsx
+++ src/containers/CardContainer/CardContainer.test.jsx
@@ -3,9 +3,9 @@ import { customRender } from "../../utils/testUtils";
 import CardContainer from "./CardContainer";
 import image from "../../assets/for-hire-image.svg"
 
-const cards = [{img: image, dateOrTime: '12:24', title: 'no', cardInfo: 'why', links: ["one", "asdf", "454"]}, 
-  {img: image, dateOrTime: '12:23', title: 'yes', cardInfo: 'why not', links: ["one", "asdf", "454"]}, 
-  {img: image, dateOrTime: '12:23', title: 'yes', cardInfo: 'why not', links: ["one", "asdf", "454"]}
+const cards = [
+  {img: image, dateOrTime: '12:01', title: 'Test title one', cardInfo: 'Test text one', links: ["one", "two", "three"]}, 
+  {img: image, dateOrTime: '12:00', title: 'Test title two', cardInfo: 'Test text two', links: ["one", "two", "three"]}, 
 ];
 
 it("it should match the snapshot with props", () => {
@@ -20,13 +20,24 @@ it("Should render the card with no props", () => {
     expect(container).toMatchSnapshot();
 });
 
-it("should render the card component on the screen", () => {
+it("should render the first card component on the screen", () => {
     render(<CardContainer cards = {cards}/>)
-    const dateOrTime = screen.getByText("12:24");
-    const title = screen.getByText("no");
-    const cardInfo = screen.getByText("why");
+    const dateOrTime = screen.getByText("12:01");
+    const title = screen.getByText("Test title one");
+    const cardInfo = screen.getByText("Test text one");
 
     expect(dateOrTime).toBeInTheDocument();
     expect(title).toBeInTheDocument();
     expect(cardInfo).toBeInTheDocument();
+});
+
+it("should render the second card component on the screen", () => {
+  render(<CardContainer cards = {cards}/>)
+  const dateOrTime = screen.getByText("12:00");
+  const title = screen.getByText("Test title two");
+  const cardInfo = screen.getByText("Test text two");
+
+  expect(dateOrTime).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+  expect(cardInfo).toBeInTheDocument();
 });
