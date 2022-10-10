ebcc19022044b76edb246d80d45cf86b39421ea3
AdrianRotila <adrianrotila@gmail.com>
2022-10-10 15:34:46 +0100

    card container ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-// import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home/Home";
-import CardContainer from "./containers/CardContainer/CardContainer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
-import image from './assets/for-hire-image.svg'
 
 const App = () => {
-
-  const cards = [{img: image,
-                  dateOrTime: '12:23',
-                  title: 'yes',
-                  cardInfo: 'why not',
-                  links: ["one", "asdf", "454"]
-                }, 
-                {img: image,
-                  dateOrTime: '12:23',
-                  title: 'yes',
-                  cardInfo: 'why not',
-                  links: ["one", "asdf", "454"]
-                }, {img: image,
-                dateOrTime: '12:23',
-                title: 'yes',
-                cardInfo: 'why not',
-                links: ["one", "asdf", "454"]
-              }];
   return (
-    // <Routes>
-    //   <Route path="*" element={<Home />} />
-    // </Routes>
-    <CardContainer cards = {cards}/>
+    <Routes>
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 };
 
