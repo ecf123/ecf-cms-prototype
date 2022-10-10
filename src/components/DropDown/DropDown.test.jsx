ed995b5ed7a7db30df0f96c37a6722b2e5892ff3
Amelia Baldwin <amelia01000010@gmail.com>
2022-10-10 16:01:40 +0100

    test added

--- src/components/DropDown/DropDown.test.jsx
+++ src/components/DropDown/DropDown.test.jsx
@@ -1,5 +1,6 @@
 import { customRender } from "../../utils/testUtils";
 import { render, screen } from "@testing-library/react";
+import { fireEvent, getByText } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import DropDown from "./DropDown";
 
@@ -18,10 +19,18 @@ it("Should check if the checkbox has been checked", () => {
 });
 
 it("Should display lesson list if checkbox is checked", () => {
-  render(<DropDown title={"Business"} lessonTitle="Financial Planning with Barclays" />);
 
-  userEvent.click(screen.getByRole("checkbox"));
-  const displayList = screen.getByRole("listitem");
+  const { container } = customRender(<DropDown title={"Business"} lessonTitle="Financial Planning with Barclays" />);
 
-  expect(displayList).toHaveStyle("height: auto");
+  let contentDiv = document.getElementsByClassName("drop-down__list");
+let style = window.getComputedStyle(contentDiv[0]);
+  expect(style.height).toBe("0px");
+
+  fireEvent.click(screen.getByRole("checkbox"));
+
+  contentDiv = document.getElementsByClassName("drop-down__list");
+  style = window.getComputedStyle(contentDiv[0]);
+  expect(style.height).toBe("auto");
+
+  
 });
