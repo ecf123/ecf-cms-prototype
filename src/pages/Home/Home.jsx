bea0b2f79cce2fddc6d4ede15ee5818a86589053
Omar Karame <omarkarmae21@gmail.com>
2022-10-10 16:17:59 +0100

    Returned home.jsx to have the layout only

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,18 @@
-// import Layout from "../../components/Layout/Layout";
-import AddPage from "../../containers/AddPage/AddPage";
+import Layout from "../../components/Layout/Layout";
 
 const Home = () => {
   return (
-    // <Layout>
-    //   <h1>Page Heading</h1>
-    //   <h2>Section Heading</h2>
-    //   <h3>Panel Heading</h3>
-    //   <p>
-    //     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-    //     dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-    //     maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
-    //     necessitatibus.
-    //   </p>
-
-
-      
-
-    // </Layout>
-
-    // shortLabelTextOne, 
-    // shortLabelTextTwo, 
-    // shortPlaceHolderTextOne, 
-    // shortPlaceHolderTextTwo, 
-    // freeTypeLabelTextOne, 
-    // freeTypeLabelTextTwo, 
-    // uploadLabelName
-
-    <AddPage 
-      shortLabelTextOne="Article Name" 
-      shortLabelTextTwo="Additional Subtitle(s) (optional)" 
-      shortPlaceHolderTextOne="e.g. Financial Forest" 
-      shortPlaceHolderTextTwo="e.g. Fund Manager" 
-      freeTypeLabelTextOne="Article Overview"
-      freeTypeLabelTextTwo="Additional Content (optional)"
-    />
-
-
-
-    
+    <Layout>
+      <h1>Page Heading</h1>
+      <h2>Section Heading</h2>
+      <h3>Panel Heading</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
+        dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
+        maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
+        necessitatibus.
+      </p>
+    </Layout>
   );
 };
 
