303d666b0cffb5e219f167eeb05cae131ef79a91
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-10-10 15:54:15 +0100

    updated id so tests run for login

--- src/components/LoginInputBox/LoginInputBox.jsx
+++ src/components/LoginInputBox/LoginInputBox.jsx
@@ -3,14 +3,14 @@ import "./LoginInputBox.scss";
 const LoginInputBox = ({ labelText }) => {
   return (
     <div className="input-box">
-      <label htmlFor="input-box" className="input-box__label">
+      <label htmlFor={labelText} className="input-box__label">
         {labelText}
       </label>
       <input
         alt="input box"
         className="input-box__input"
         type="text"
-        id="input-box"
+        id={labelText}
       />
     </div>
   );
