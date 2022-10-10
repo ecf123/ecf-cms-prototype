b9bfd4ee7d70ec006415927daccb51a543dddded
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-10-10 16:59:12 +0100

    added test

--- src/components/ListComponentUniversal/ListComponentUniversal.test.jsx
+++ src/components/ListComponentUniversal/ListComponentUniversal.test.jsx
@@ -1,28 +1,37 @@
 import { render, screen } from "@testing-library/react";
-import { customRender } from "../../utils/testUtils";
-import ListComponentUniversal from "./ListComponentUniversal";
-
-// const columns = [
-//   {
-//     title: "NO OF COURSES",
-//     dataIndex: "no_of_courses",
-//   },
-//   {
-//     title: "NO OF LESSONS",
-//     dataIndex: "no_of_lessons",
-//   },
-//   {
-//     title: "EST. COMPLETION TIME",
-//     dataIndex: "est_completion_time",
-//   },
-// ]
-
-// const data=[{
-//     no_of_courses: `8`,
-//     no_of_lessons: `8`,
-//     est_completion_time: `20hrs`,
-//   }];
-
-it ("should render the list component universal with multiple columns and rows", ()=> {
-
+import { Table } from "antd";
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
+it("should render the list component universal with multiple columns and rows", () => {
+  const columns = [
+    {
+      title: "NO OF COURSES",
+      dataIndex: "no_of_courses",
+    },
+    {
+      title: "NO OF LESSONS",
+      dataIndex: "no_of_lessons",
+    },
+    {
+      title: "EST. COMPLETION TIME",
+      dataIndex: "est_completion_time",
+    },
+  ];
+  const data = [
+    {
+      no_of_courses: `8`,
+      no_of_lessons: `8`,
+      est_completion_time: `20hrs`,
+    },
+  ];
+  render(<Table columns={columns} dataSource={data} />);
+  const cell = screen.getByText("20hrs");
+  expect(cell).toBeInTheDocument();
 });
