db7c10dd496cd32fe790f6908d694e24403fec69
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-10 16:52:15 +0100

    merging main

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
+
     const email = e.target.elements[0].value;
     const password = e.target.elements[1].value;
 
@@ -18,39 +18,56 @@ const Login = () => {
       setHasError(true);
       return;
     }
-    
+
     setHasError(false);
     setHasSubmitted(true);
   };
 
   return (
-    <> 
-     {!hasError && hasSubmitted && <div>
+    <>
+      {!hasError && hasSubmitted && (
+        <div>
           <p data-testid="success" className="login__success">
             Thank you for submitting! We will be in touch
           </p>
-        </div> }
-      {!hasSubmitted && 
-        <form data-testid="form" onSubmit={handleSubmit} name="form" className="login">
+        </div>
+      )}
+      {!hasSubmitted && (
+        <form
+          data-testid="form"
+          onSubmit={handleSubmit}
+          name="form"
+          className="login"
+        >
           <div className="login__title">
             <h1 className="login__title--heading">Welcome Back</h1>
-            <p className='login__title--text'>Feugiat magna viverra pellentesque habitant. Libero id amet nisl, tellus cras eleifend et in. </p>
+            <p className="login__title--text">
+              Feugiat magna viverra pellentesque habitant. Libero id amet nisl,
+              tellus cras eleifend et in.{" "}
+            </p>
           </div>
-          <div >
-            <LoginInputBox labelText="Email Address" />
-            <LoginInputBox labelText="Password"/>
+          <div>
+            <LoginInputBox
+              inputBoxStyle="input-box__input short"
+              labelText="Email Address"
+            />
+            <LoginInputBox
+              inputBoxStyle="input-box__input short"
+              labelText="Password"
+            />
           </div>
           <Button
             style={"button login-black square-border"}
             textStyle={"text medium-text black"}
             buttonText={"Login"}
           />
-          {hasError && <div className="login__alert">
-          <p>Sorry something went wrong </p> </div>}
-        </form>}
-         
-      
-    
+          {hasError && (
+            <div className="login__alert">
+              <p>Sorry something went wrong </p>{" "}
+            </div>
+          )}
+        </form>
+      )}
     </>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,7 @@
 import Layout from "../../components/Layout/Layout";
-<<<<<<< HEAD
-import Login from "../../components/Login/Login";
-=======
 
->>>>>>> main
 const Home = () => {
- return(
-   <Layout>
-    <Login />
-   </Layout>
- )
-}
+  return <Layout></Layout>;
+};
 
-export default Home
-
+export default Home;
