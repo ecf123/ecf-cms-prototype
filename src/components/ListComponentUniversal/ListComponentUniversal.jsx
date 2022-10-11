fd294bd11af6640e3c0ba5ab318b38999fc757af
michal <michalwagh@gmail.com>
2022-10-11 19:46:13 +0100

    for debugging

--- src/components/ListComponentUniversal/ListComponentUniversal.jsx
+++ src/components/ListComponentUniversal/ListComponentUniversal.jsx
@@ -2,13 +2,47 @@ import "antd/dist/antd.css";
 import "./ListComponentUniversal.scss";
 import { Table } from "antd";
 import { useState } from "react";
+// import { ReactComponent as Barclays } from "../../assets/barclays-icon-white-background.svg";
+import Airbnb from "../../assets/airbnb-logo.svg";
+// import { render } from "@testing-library/react/types
+// import { render } from "react-dom";
 
-const ListComponentUniversal = ({columns, data}) => {
+const ListComponentUniversal = ({ columns, data }) => {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const onSelectChange = (newSelectedRowKeys) => {
     console.log("selectedRowKeys changed: ", selectedRowKeys);
     setSelectedRowKeys(newSelectedRowKeys);
   };
+
+  // data.map((element) => {
+  //   // console.log(element);
+  //   if (element.providersIcon) {
+  //     element.providersIconTag = [];
+  //     element.providersIcon.forEach((icon) => {
+  //       console.log(icon);
+  //       element.providersIconTag.push(<img src={icon} className="providers" />);
+  //       // icon = <img src={icon} className="providers" />;
+  //     });
+  //   }
+  //   return element;
+  // });
+
+  data = data.map((element) => {
+    if (element.providersIcon) {
+      element.providersIcon.map((icon, index) => {
+        console.log(icon);
+        // render: () => {
+        return (
+          <div key={index}>
+            <img src={Airbnb} className="providers" alt="icon" />
+          </div>
+        );
+        // };
+      });
+    }
+    return element;
+  });
+
   const rowSelection = {
     selectedRowKeys,
     onChange: onSelectChange,
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,54 @@
 import Layout from "../../components/Layout/Layout";
 import ListComponentUniversal from "../../components/ListComponentUniversal/ListComponentUniversal";
 import { Modal, Input } from "antd";
-// import { ReactComponent as Barclays } from "../../assets/barclays-icon-white-background.svg";
-//  import { ReactComponent as Airbnb } from "../../assets/airbnb-logo.svg";
+// import Barclays from "../../assets/barclays-icon-white-background.svg";
+import Airbnb from "../../assets/airbnb-logo.svg";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import { useState } from "react";
 
+// const textToImage = (image) => {
+//   return <img src={image} className="providers" />;
+// };
+
 const data = [];
-  for (let i = 0; i < 5; i++) {
-    data.push({
-      key: i,
-      id: i,
-      pathway_name: `Financial Forest + ${i}`,
-      providers: `src`,
-      no_of_courses: `8`,
-      no_of_lessons: `8`,
-      est_completion_time: `20hrs`,
-      no_enrolled: 32,
-      category: `management`,
-    });
-  }
+for (let i = 0; i < 5; i++) {
+  data.push({
+    key: i,
+    id: i,
+    pathway_name: `Financial Forest + ${i}`,
+    providersIcon: [
+      Airbnb,
+      Airbnb,
+      // textToImage(Airbnb),
+      // textToImage(Airbnb),
+      // textToImage(Airbnb),
+    ],
+    no_of_courses: `8`,
+    no_of_lessons: `8`,
+    est_completion_time: `20hrs`,
+    no_enrolled: 32,
+    category: `management`,
+  });
+}
 
 const Home = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [editingEntry, setEditingEntry] = useState(null);
-  const [dataSource, setDataSource] = useState(data)
+  const [dataSource, setDataSource] = useState(data);
 
   const columns = [];
 
-  Object.keys(data[0]).forEach(header => {
-    if (header != "id" && header != "key") columns.push({title: header, dataIndex: header})
-  })
-  
+  Object.keys(data[0]).forEach((header) => {
+    // if (header == "providersIcon") {
+    // }
+
+    if (header != "id" && header != "key")
+      columns.push({
+        title: header.replace(/_/g, " ").toUpperCase(),
+        dataIndex: header,
+      });
+  });
+
   // const jsx = Object.keys(data[0]).map((header, index) => {
   //   if (header != "id" && header != "key"){
   //   return (
@@ -46,18 +63,18 @@ const Home = () => {
   //   />)
   //   }
   // });
-  
 
   columns.push({
     key: "",
-    title: "Actions",
+    title: "",
     render: (e) => {
       return (
         <>
-          <EditOutlined 
-          onClick={() => {
-            onEditStudent(e);
-          }}/>
+          <EditOutlined
+            onClick={() => {
+              onEdit(e);
+            }}
+          />
           <DeleteOutlined
             onClick={() => {
               onDelete(e);
@@ -67,30 +84,30 @@ const Home = () => {
         </>
       );
     },
-  },)
-
-const onDelete = (e) => {
-  Modal.confirm({
-    title: "Are you sure, you want to delete this record?",
-    okText: "Yes",
-    okType: "danger",
-    onOk: () => {
-      setDataSource((pre) => {
-        return pre.filter((row) => row.id !== e.id);
-      });
-    },
   });
-};
 
-const onEditStudent = (e) => {
-  setIsEditing(true);
-  setEditingEntry({ ...e });
-};
-const resetEditing = () => {
-  setIsEditing(false);
-  setEditingEntry(null);
-};
-//  for
+  const onDelete = (e) => {
+    Modal.confirm({
+      title: "Are you sure, you want to delete this record?",
+      okText: "Yes",
+      okType: "danger",
+      onOk: () => {
+        setDataSource((pre) => {
+          return pre.filter((row) => row.id !== e.id);
+        });
+      },
+    });
+  };
+
+  const onEdit = (e) => {
+    setIsEditing(true);
+    setEditingEntry({ ...e });
+  };
+  const resetEditing = () => {
+    setIsEditing(false);
+    setEditingEntry(null);
+  };
+  //  for
   return (
     <Layout>
       {/* <h1>Page Heading</h1>
@@ -102,48 +119,47 @@ const resetEditing = () => {
         maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
         necessitatibus.
       </p> */}
-      <ListComponentUniversal columns={columns} data={dataSource}/>
-        <Modal
-          title="Edit Entry"
-          open={isEditing}
-          okText="Save"
-          onCancel={() => {
-            resetEditing();
-          }}
-          onOk={() => {
-            setDataSource((pre) => {
-              return pre.map((e) => {
-                if (e.id === editingEntry.id) {
-                  return editingEntry;
-                } else {
-                  return e;
-                }
-              });
+      <ListComponentUniversal columns={columns} data={dataSource} />
+      <img src={Airbnb} />
+      <Modal
+        title="Edit Entry"
+        open={isEditing}
+        okText="Save"
+        onCancel={() => {
+          resetEditing();
+        }}
+        onOk={() => {
+          setDataSource((pre) => {
+            return pre.map((e) => {
+              if (e.id === editingEntry.id) {
+                return editingEntry;
+              } else {
+                return e;
+              }
             });
-            resetEditing();
+          });
+          resetEditing();
+        }}
+      >
+        <Input
+          value={editingEntry?.pathway_name}
+          onChange={(e) => {
+            setEditingEntry((pre) => {
+              return { ...pre, pathway_name: e.target.value };
+            });
           }}
-        >
-          <Input
-            value={editingEntry?.pathway_name}
-            onChange={(e) => {
-              setEditingEntry((pre) => {
-                return { ...pre, pathway_name: e.target.value };
-              });
-            }}
-          />
-          <Input
-            value={editingEntry?.no_of_courses}
-            onChange={(e) => {
-              setEditingEntry((pre) => {
-                return { ...pre, no_of_courses: e.target.value };
-              });
-            }}
-          />
-
-        </Modal>
+        />
+        <Input
+          value={editingEntry?.no_of_courses}
+          onChange={(e) => {
+            setEditingEntry((pre) => {
+              return { ...pre, no_of_courses: e.target.value };
+            });
+          }}
+        />
+      </Modal>
     </Layout>
   );
 };
 
 export default Home;
-
