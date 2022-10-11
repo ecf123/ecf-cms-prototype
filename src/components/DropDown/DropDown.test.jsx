a257a231803008d257996212683d35e5d03ff43e
Amelia Baldwin <amelia01000010@gmail.com>
2022-10-11 19:17:21 +0100

    tests fixed for refactored DropDown

--- src/components/DropDown/DropDown.test.jsx
+++ src/components/DropDown/DropDown.test.jsx
@@ -3,15 +3,15 @@ import { render, screen } from "@testing-library/react";
 import { fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import DropDown from "./DropDown";
-
+const lessonArr = [{title: "The role of banking in the financial system"}, {title: "Currency Risk Management"}];
 it("Should render DropDown component", () => {
-  const { container } = customRender(<DropDown title={"Business"} lessonTitle="Financial Planning with Barclays" />);
+  const { container } = customRender(<DropDown title={"Business"} lessonArr={lessonArr} />);
 
   expect(container).toMatchSnapshot();
 });
 
 it("Should check if the checkbox has been checked", () => {
-  render(<DropDown title={"Business"} lessonTitle="Financial Planning with Barclays" />);
+  render(<DropDown title={"Business"} lessonArr={lessonArr} />);
 
   userEvent.click(screen.getByRole("checkbox"));
 
@@ -19,15 +19,15 @@ it("Should check if the checkbox has been checked", () => {
 });
 
 it("Should display lesson list if checkbox is checked", () => {
-  render(<DropDown title={"Business"} lessonTitle="Financial Planning with Barclays" />);
+  render(<DropDown title={"Business"} lessonArr={lessonArr}  />);
 
-  const list = screen.getByRole("listitem");
+  const list = screen.getByText("The role of banking in the financial system");
 
   expect(list).toHaveStyle({ height: " 0" });
 
   fireEvent.click(screen.getByRole("checkbox"));
 
-  const listAfter = screen.getByRole("listitem");
+  const listAfter = screen.getByText("The role of banking in the financial system");
 
   expect(listAfter).toHaveStyle({ height: " auto" });
 });
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,7 @@
 import DropDown from "../DropDown/DropDown";
 import "./Layout.scss";
 const Layout = ({ children }) => {
-  const lessonArr = [{title: "The role of banking in the financial system"}, {title: "Currency Risk Management"}];
-
-
-  return <div className="layout"><DropDown topicTitle={"Business"} lessonArr={lessonArr} topicInput={"click"}/>
-  {children}</div>;
+  return <div className="layout">{children}</div>;
 };
 
 export default Layout;
