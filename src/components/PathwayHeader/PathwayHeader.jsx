c7e9b258bfac4e6755d726e6ed35f73b72ebf2f6
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-11 14:16:20 +0100

    made button link to new page

--- src/components/PathwayHeader/PathwayHeader.jsx
+++ src/components/PathwayHeader/PathwayHeader.jsx
@@ -1,19 +1,21 @@
 import "./PathwayHeader.scss";
 import Button from "../Button/Button";
 import { ReactComponent as Pencil } from "../../assets/pen-icon.svg";
-const PathwayHeader = ({ addCourse }) => {
+import { Link } from "react-router-dom";
+const PathwayHeader = ({ link }) => {
   return (
     <div className="pathway-header">
       <h2 className="pathway-header__title">Pathway Structure</h2>
-      <Button
-        buttonFunction={addCourse}
-        style={"button black round-border medium"}
-        imgStyle={"medium-img white-svg"}
-        textStyle={"text medium-text black"}
-        buttonText={"Add Course"}
-      >
-        <Pencil />
-      </Button>
+      <Link to={link}>
+        <Button
+          style={"button black round-border medium"}
+          imgStyle={"medium-img white-svg"}
+          textStyle={"text medium-text black"}
+          buttonText={"Add Course"}
+        >
+          <Pencil />
+        </Button>
+      </Link>
     </div>
   );
 };
