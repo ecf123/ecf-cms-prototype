4d78157af17a4344579bdeea5304fad10ba22d9a
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-12 14:44:14 +0100

    npm run format

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,16 +1,12 @@
 import "./Overview.scss";
 
-
-const Overview = ({header, text}) => {
-
-
+const Overview = ({ header, text }) => {
   return (
     <div className="overview">
-        <h2 className="overview__header">{header}</h2>
-        <p className="overview__text">{text}</p>
+      <h2 className="overview__header">{header}</h2>
+      <p className="overview__text">{text}</p>
     </div>
+  );
+};
 
-  )
-}
-
-export default Overview
+export default Overview;
--- src/components/Overview/Overview.test.jsx
+++ src/components/Overview/Overview.test.jsx
@@ -1,7 +1,6 @@
 import { customRender } from "../../utils/testUtils";
-import {render, screen} from "@testing-library/react";
-import Overview from "./Overview"
-
+import { render, screen } from "@testing-library/react";
+import Overview from "./Overview";
 
 it("renders the Overview", () => {
   const { container } = customRender(<Overview />);
@@ -9,16 +8,15 @@ it("renders the Overview", () => {
 });
 
 it("should render the header on the screen", () => {
-  render(<Overview header="Overview"/>)
+  render(<Overview header="Overview" />);
   const header = screen.getByText("Overview");
 
   expect(header).toBeInTheDocument();
-})
+});
 
 it("should render the text on the screen", () => {
-  render(<Overview text="text"/>)
+  render(<Overview text="text" />);
   const text = screen.getByText("text");
 
   expect(text).toBeInTheDocument();
-})
-
+});
