ed0744b32d1ad1c853f82053512715677cbd304a
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-12 16:18:57 +0100

    put components on page

--- src/containers/ListContainer/ListContainer.jsx
+++ src/containers/ListContainer/ListContainer.jsx
@@ -1,8 +1,24 @@
 import "./ListContainer.scss"
+import PageTitle from "../../components/PageTitle/PageTitle";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Button from "../../components/Button/Button"
+import CardContainer from "../CardContainer/CardContainer"
+import {ReactComponent as Pencil} from "../../assets/pen-icon.svg";
 
-const ListContainer = () => {
+const ListContainer = ({title, cards}) => {
   return (
-    <div>ListContainer</div>
+    <div className="list-page">
+      <div className="list-page__header">
+        <PageTitle className="list-page__title" title={title+" List"} />
+      </div>
+      <div className="list-page__body">
+        <nav className="list-page__nav">
+          <SearchBar placeHolderText={"Search for "+title}/>
+          <Button style={"button black round-border medium"} imgStyle={"medium-img white-svg"} textStyle={"text medium-text black"} buttonText={"Add "+title}><Pencil /></Button>
+        </nav>
+        <CardContainer cards={cards}/>
+      </div>
+    </div>
   )
 }
 
