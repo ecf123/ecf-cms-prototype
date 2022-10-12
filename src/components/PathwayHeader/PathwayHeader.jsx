d9541d6790a3d6db25c1fc81c408622f25d93f1d
Connel Nwaodor <connel_n@hotmail.com>
2022-10-12 14:27:10 +0100

    Changes reverted

--- src/components/PathwayHeader/PathwayHeader.jsx
+++ src/components/PathwayHeader/PathwayHeader.jsx
@@ -1,5 +1,4 @@
 import "./PathwayHeader.scss";
-<<<<<<< HEAD
 
 
 
@@ -31,27 +30,3 @@ const pathwayTags = ["Finance", "Investing"]
 }
 
 export default PathwayHeader
-=======
-import Button from "../Button/Button";
-import { ReactComponent as Pencil } from "../../assets/pen-icon.svg";
-import { Link } from "react-router-dom";
-const PathwayHeader = ({ link }) => {
-  return (
-    <div className="pathway-header">
-      <h2 className="pathway-header__title">Pathway Structure</h2>
-      <Link to={link}>
-        <Button
-          style={"button black round-border medium"}
-          imgStyle={"medium-img white-svg"}
-          textStyle={"text medium-text black"}
-          buttonText={"Add Course"}
-        >
-          <Pencil />
-        </Button>
-      </Link>
-    </div>
-  );
-};
-
-export default PathwayHeader;
->>>>>>> a96090cdbf9b6af96ce8528bf3e7d0c611aa9ae5
