2d2a93a78fe5d1d454b0794ffa638d88ba72c9d0
Chen <liangchen_amelia@163.com>
2022-10-12 14:42:17 +0100

    deal with the comment

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,13 +1,12 @@
 import "./Overview.scss";
 
-import React from 'react'
 
 const Overview = ({header, text}) => {
 
 
   return (
     <div className="overview">
-        <h2 className="overview__header">{header} Overview</h2>
+        <h2 className="overview__header">{header}</h2>
         <p className="overview__text">{text}</p>
     </div>
 
