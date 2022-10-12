41375c26f19811df60807e787b7ac86769c05c5d
atif298 <atifuddin03@gmail.com>
2022-10-12 16:12:59 +0100

    Display

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import headingImage from "./assets/mobile-growing-investment.svg"
 import companyIcon from "./assets/barclays-icon-white-background.svg"
 
 const App = () => {
+
+  
+const links = ["Finance", "Investing"];
+
   return (
     <Routes>
       <Route path="*" element={<PathwayHeader headingTitle = "Commercial Banking with Barclay's" 
