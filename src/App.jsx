7fdee8b3ba141e87f74df3baccf163d8edb7e398
AdrianRotila <adrianrotila@gmail.com>
2022-10-12 15:33:36 +0100

    added styles to the card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import PathwayHeader from "./components/PathwayHeader/PathwayHeader";
+// import Home from "./pages/Home/Home";
 import "./styles/main.scss";
+import headingImage from "./assets/mobile-growing-investment.svg"
+import companyIcon from "./assets/barclays-icon-white-background.svg"
 
 const App = () => {  
+
+  const links = ["Finance", "Investing"];
   return (  
     <Routes>
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<PathwayHeader headingTitle = "Commercial Banking with Barclay's" 
+      headingImage = {headingImage} companyIcon = {companyIcon} links = {links} condition = {false}/>} />
     </Routes>
   );
 };
--- src/components/PathwayHeader/PathwayHeader.jsx
+++ src/components/PathwayHeader/PathwayHeader.jsx
@@ -1,15 +1,16 @@
 import "./PathwayHeader.scss";
+import CategoriesMenu from '../CategoriesMenu/CategoriesMenu'
 
-const PathwayHeader = ({headingTitle, headingImage, companyIcon }) => {
-    return (
-        <div className="heading">
-            <div className="heading__banner">
-                <img className="heading__icon" src={companyIcon} alt="picture of company icon" />
-                <img className="heading__image" src={headingImage} alt="picture of pathway heading" />
-            </div>
-            <h2>{headingTitle}</h2>
-        </div>
-    )
+const PathwayHeader = ({headingTitle, headingImage, companyIcon, links, condition }) => {
+  
+  return (
+    <div className="heading">
+        <img className="heading__icon" src={companyIcon} alt="picture of company icon" />
+        <img className="heading__image" src={headingImage} alt="picture of pathway heading" />
+        <h1 className="heading__title">{headingTitle}</h1>
+        <CategoriesMenu links = {links}  condition = {condition}/>
+    </div>   
+  )
 }
 
 export default PathwayHeader
