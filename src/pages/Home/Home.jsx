efba1c92b02bd72cf754c4b70dee0686da1a390d
Omar Karame <omarkarmae21@gmail.com>
2022-10-13 16:49:29 +0100

    Returned home.jsx to its original code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,18 @@
-// import Layout from "../../components/Layout/Layout";
-import AddPage from "../../containers/AddPage/AddPage";
+import Layout from "../../components/Layout/Layout";
 
 const Home = () => {
   return (
-    // <Layout>
-    //   <h1>Page Heading</h1>
-    //   <h2>Section Heading</h2>
-    //   <h3>Panel Heading</h3>
-    //   <p>
-    //     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
-    //     dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
-    //     maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
-    //     necessitatibus.
-    //   </p>
-    // </Layout>
-    <AddPage 
-    shortLabelTextOne="Input 1"
-    shortLabelTextTwo="Input 3" 
-    freeTypeLabelTextOne="Input 2 (optional)"
-    freeTypeLabelTextTwo="Input 4"
-    />
+    <Layout>
+      <h1>Page Heading</h1>
+      <h2>Section Heading</h2>
+      <h3>Panel Heading</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non error
+        dolor in nostrum minima odio a fuga saepe soluta adipisci perspiciatis
+        maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
+        necessitatibus.
+      </p>
+    </Layout>
   );
 };
 
