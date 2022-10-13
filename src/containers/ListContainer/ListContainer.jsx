6a1f7453030b470338abbe579fd6a7a3c297e8a9
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-13 15:25:12 +0100

    search function

--- src/containers/ListContainer/ListContainer.jsx
+++ src/containers/ListContainer/ListContainer.jsx
@@ -4,8 +4,18 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import Button from "../../components/Button/Button"
 import CardContainer from "../CardContainer/CardContainer"
 import {ReactComponent as Pencil} from "../../assets/pen-icon.svg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const ListContainer = ({title, cards}) => {
+  const [ searchTerm , setSearchTerm ] = useState("");
+
+  const handleSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  const filteredCards = cards?.filter((card) => card.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return (
     <div className="list-page">
       <div className="list-page__header">
@@ -13,10 +23,10 @@ const ListContainer = ({title, cards}) => {
       </div>
       <div className="list-page__body">
         <nav className="list-page__nav">
-          <SearchBar placeHolderText={"Search for "+title}/>
-          <Button style={"button black round-border large"} imgStyle={"medium-img white-svg"} textStyle={"text medium-text black"} buttonText={"Add "+title}><Pencil /></Button>
+          <SearchBar placeHolderText={"Search for "+title} handleSearchInput={handleSearchInput} value={searchTerm}/>
+          <Link to="*"><Button style={"button black round-border large"} imgStyle={"medium-img white-svg"} textStyle={"text medium-text black"} buttonText={"Add "+title}><Pencil /></Button></Link>
         </nav>
-        <CardContainer cards={cards}/>
+        <CardContainer cards={filteredCards}/>
       </div>
     </div>
   )
