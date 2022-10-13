a9e412dc02f1a5238fe218893ed6b5aceb92a034
AdrianRotila <adrianrotila@gmail.com>
2022-10-13 16:28:15 +0100

    created the html items

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import AddQuestion from "./components/AddQuestion/AddQuestion";
+// import Home from "./pages/Home/Home";
 import "./styles/main.scss";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<AddQuestion />} />
     </Routes>
   );
 };
---
+++ src/components/AddQuestion/AddQuestion.jsx
@@ -0,0 +1,18 @@
+import './AddQuestion.scss'
+
+const AddQuestion = () => {
+
+  return (
+    <div>
+        <h4>Q1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+        <ol type='A'>
+            <li>Answer A</li>
+            <li>Answer B</li>
+            <li>Answer C</li>
+            <li>Answer D</li>
+        </ol>
+    </div>
+  )
+}
+
+export default AddQuestion
