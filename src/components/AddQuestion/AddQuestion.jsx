997dda0840bf84538e5d2c2d762c1689a3869943
atif298 <atifuddin03@gmail.com>
2022-10-13 16:56:50 +0100

    Button div

--- src/components/AddQuestion/AddQuestion.jsx
+++ src/components/AddQuestion/AddQuestion.jsx
@@ -1,9 +1,13 @@
 import './AddQuestion.scss'
+// import Button from "../Button/Button"
+import binIcon from "../../assets/bin-icon.svg"
+import penIcon from "../../assets/pen-icon.svg"
 
 const AddQuestion = () => {
 
   return (
     <div>
+      <div className='questions'>
         <h4>Q1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
         <ol type='A'>
             <li>Answer A</li>
@@ -11,6 +15,21 @@ const AddQuestion = () => {
             <li>Answer C</li>
             <li>Answer D</li>
         </ol>
+        </div>
+        <div className='buttons'>
+          <Button style={}
+                  textStyle={}
+                  buttonText={}
+                  children={}
+                  imgStyle={}
+                  buttonFunction={}/>
+          <Button style={}
+                  textStyle={}
+                  buttonText={}
+                  children={}
+                  imgStyle={}
+                  buttonFunction={}/>
+        </div>
     </div>
   )
 }
