dfa3f20f86093242c5ea7bb657c9973920e9011f
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-10-14 16:06:31 +0100

    added toggle function for different display styles

--- src/components/PathwayDisplay/PathwayDisplay.jsx
+++ src/components/PathwayDisplay/PathwayDisplay.jsx
@@ -7,11 +7,20 @@ import { Link } from "react-router-dom";
 
 
 const PathwayDisplay = () => {
+  let view = "grid";
+  const toggleGrid = () => {
+    view = "grid"
+    console.log(view)
+  }
+  const toggleList = () => {
+    view = "list"
+    console.log(view)
+  }
   return (
     <div className="pathway-display">
         <div className="pathway-display__container">
-          <img className="pathway-display__option" id="grid-icon" src={grid} alt="grid display option"/>
-          <img className="pathway-display__option" src ={hamburger} alt="list view option"/>
+          <img className="pathway-display__option" id="grid-icon" src={grid} alt="grid display option button" onClick={toggleGrid}/>
+          <img className="pathway-display__option" src ={hamburger} alt="list view option button" onClick={toggleList}/>
         </div>
         <Link to = "*" className="pathway-display__button">
           < Button style={"button black round-border medium"} imgStyle={"medium-img white-svg"} textStyle={"text medium-text black"} buttonText={"Add Course"}><Pencil /></Button>
