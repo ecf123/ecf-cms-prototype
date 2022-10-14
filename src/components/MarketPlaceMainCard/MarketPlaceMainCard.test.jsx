1ce832136a35720881017f2e2aa809cb16e3cef1
lucygilhooly <110402198+lucygilhooly@users.noreply.github.com>
2022-10-14 15:17:11 +0100

    97 pathway view options component (#60)
    * made viewOptions component
    
    * changed the text prop
    
    * removed unnecessary styling
    
    * clciking on each option working
    
    * long line added -very cool
    
    * line position moved
    
    * added tests, all passing
    
    * deleted from home
    
    * re-rendered and removed from home
    
    Co-authored-by: Amelia Baldwin <amelia01000010@gmail.com>
    Co-authored-by: Brooke Waldorf-Wells <101236557+BrookeVerse@users.noreply.github.com>

--- src/components/MarketPlaceMainCard/MarketPlaceMainCard.test.jsx
+++ src/components/MarketPlaceMainCard/MarketPlaceMainCard.test.jsx
@@ -14,7 +14,7 @@ it("should render every marketPlace main card prop", () => {
       condition="true"
     />
   );
-  
+
   const imgDescription = screen.getByAltText("Test alt text");
   const date = screen.queryByText("22/01/2022");
   const header = screen.queryByText("Test Title");
@@ -27,17 +27,20 @@ it("should render every marketPlace main card prop", () => {
 });
 
 it("should render an image in the marketplace main card", () => {
-  render(<MarketPlaceMainCard 
-  img={rollercoaster}  
-  imgDescription="Test alt text"
-  date="22/01/2022"
-  title="Test Title"
-  articleInfo="Test article"
-  links={["Word", "Text"]}
-  condition="true"/>);
-  const imgDescription = screen.queryByAltText("Test alt text")
+  render(
+    <MarketPlaceMainCard
+      img={rollercoaster}
+      imgDescription="Test alt text"
+      date="22/01/2022"
+      title="Test Title"
+      articleInfo="Test article"
+      links={["Word", "Text"]}
+      condition="true"
+    />
+  );
+  const imgDescription = screen.queryByAltText("Test alt text");
   expect(imgDescription).toBeTruthy();
-})
+});
 
 it("should render marketPlace main card with children", () => {
   const { container } = customRender(
