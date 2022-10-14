b38042ce13365e11181d2fc8270b8fe637a8c19a
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-10-14 16:51:33 +0100

    added all tests and snapshot test, all tests are passing

--- src/components/PathwayDisplay/PathwayDisplay.test.jsx
+++ src/components/PathwayDisplay/PathwayDisplay.test.jsx
@@ -1,16 +1,32 @@
 import { render, screen } from "@testing-library/react";
 import { customRender } from "../../utils/testUtils";
-
+import { BrowserRouter } from "react-router-dom";
 import PathwayDisplay from "./PathwayDisplay.jsx";
 
 it("should render the grid icon", () => {
-    render(<PathwayDisplay />);
+    render(<PathwayDisplay />, {wrapper: BrowserRouter});
     const image = screen.getByAltText("grid display option button");
     expect(image).toBeInTheDocument();
   });
 
   it("should render the list icon", () => {
-    render(<PathwayDisplay />);
+    render(<PathwayDisplay />, {wrapper: BrowserRouter});
     const image = screen.getByAltText("list view option button");
     expect(image).toBeInTheDocument();
   });
+
+  it("Should render the add course button", () => {
+    render(<PathwayDisplay />, {wrapper: BrowserRouter});
+    const button = screen.getByText("Add Course");
+    expect(button).toBeInTheDocument();
+  })
+
+  it("Should render Pathway Display", () => {
+    const {container} = customRender(
+      <PathwayDisplay/>, {wrapper: BrowserRouter}
+    )
+    expect(container).toMatchSnapshot();
+
+  })
+
+
