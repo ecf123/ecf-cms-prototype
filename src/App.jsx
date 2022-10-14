3fa37a88cb61e58cbc640aaefc7f44fc93450d46
atif298 <atifuddin03@gmail.com>
2022-10-14 16:41:54 +0100

    Styling complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import "./styles/main.scss";
 
 const App = () => {
 
-  const answer = ["Adrian", "Atif", "Andy", "Connel", "Connel *"];
-  const question = "What is my name"
-  const questionNumber = 1
+  const answer = ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"];
+  const question = "Which Product does Adrian sell the most on Amazon"
+  const questionNumber = 7
 
   return (
     <Routes>
