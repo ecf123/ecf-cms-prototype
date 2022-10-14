bf05cdc733ad2edf1aba886a3b663599b56741c1
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-14 16:49:50 +0100

    rendered to home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
 import Layout from "../../components/Layout/Layout";
+import HeaderContainer from "../../containers/HeaderContainer/HeaderContainer";
 
 const Home = () => {
-  return <Layout></Layout>;
+  return <Layout>
+    <HeaderContainer classNameSmall={"page-title--small"} classNameLarge={"page-title--large"} titleSmall={"Pathways"} titleLarge={"Financial Forest"} profileImg={"man"} name={"John Doe"} role={"Dev"} toggle={"toggle"} />
+  </Layout>;
 };
 
 export default Home;
