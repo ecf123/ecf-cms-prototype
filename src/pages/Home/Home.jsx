5f9f66165dfc8c4b2b929ac18a725be545e4147c
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-14 16:56:36 +0100

    rendered in home again

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,8 @@ import HeaderContainer from "../../containers/HeaderContainer/HeaderContainer";
 
 const Home = () => {
   return <Layout>
-<<<<<<< HEAD
-    <HeaderContainer classNameSmall={"page-title--small"} classNameLarge={"page-title--large"} titleSmall={"Pathways"} titleLarge={"Financial Forest"} profileImg={"man"} name={"John Doe"} role={"Dev"} toggle={"toggle"} />
-=======
->>>>>>> a67113c3407128d5b976fa2e8388dfe9a18e25df
+        <HeaderContainer classNameSmall={"page-title--small"} classNameLarge={"page-title--large"} titleSmall={"Pathways"} titleLarge={"Financial Forest"} profileImg={"man"} name={"John Doe"} role={"Dev"} toggle={"toggle"} />
+
   </Layout>;
 };
 
