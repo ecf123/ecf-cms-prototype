f22260a86db895897500ec58bb510eceda835c31
michal <michalwagh@gmail.com>
2022-10-14 15:37:44 +0100

    cleaned App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Short from "./components/Short/Short";
 import "./styles/main.scss";
 
 const App = () => {
   return (
     <Routes>
-      <Route
-        path="/FinancialForest+1"
-        element={<Short freeTypeLabelText="hello" />}
-      />
       <Route path="*" element={<Home />} />
     </Routes>
   );
