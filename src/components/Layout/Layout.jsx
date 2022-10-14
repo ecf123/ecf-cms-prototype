74f574af92faf30b7e673210c515a80f652638e6
Chen <liangchen_amelia@163.com>
2022-10-14 14:54:08 +0100

    build up the component and finish styling

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,31 @@
 import "./Layout.scss";
+import NavBar from  "../NavBar/NavBar"
+import HeaderProfile from "../HeaderProfile/HeaderProfile"
+
+
 const Layout = ({ children }) => {
-  return <div className="layout">{children}</div>;
+  return (
+  
+  <div className="layout">
+
+    <div className="layout__nav-bar">
+      <NavBar />
+    </div>
+
+    <div className="layout__header">
+      <h1 className="layout__header--title">Header</h1>
+      <HeaderProfile />
+    </div>
+
+
+    <div>
+      {children}
+    </div>
+    
+    
+  
+  </div>
+  )
 };
 
 export default Layout;
