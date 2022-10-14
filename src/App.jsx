4bde01807f3ad974f1a9126fc785b4dec34b4e52
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-10-14 14:44:16 +0100

    added grid and list display icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./pages/Home/Home";
 import "./styles/main.scss";
+import hamburger from "./assets/holding-card.svg";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="*" element={<Home />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="*" element={<Home />} />
+      
+    // </Routes>
+    <img src={hamburger}/>
   );
 };
 
--- src/components/PathwayDisplay/PathwayDisplay.jsx
+++ src/components/PathwayDisplay/PathwayDisplay.jsx
@@ -1,9 +1,16 @@
 import "./PathwayDisplay.scss";
+import hamburger from "../../assets/hamburger-icon.svg";
+import grid from "../../assets/grid-icon.svg";
+import Button from "../Button/Button";
 
 const PathwayDisplay = () => {
   return (
     <div>
-        
+        <div>
+          <img src ={hamburger} alt="list view option"/>
+          <img src={grid} alt="grid display option"/>
+        </div>
+        <Button style="black" textStyle={} />
     </div>
   )
 }
