714d0066f644dd35b6589c8746da676e119c09b0
atif298 <atifuddin03@gmail.com>
2022-10-17 15:31:30 +0100

    Base files added with styling

---
+++ src/containers/QuestionContainer/QuestionContainer.jsx
@@ -0,0 +1,14 @@
+import "QuestionContainer.scss"
+
+const QuestionContainer = () => {
+
+
+
+  return (
+    <div className="questions">
+        
+    </div>
+  )
+}
+
+export default QuestionContainer
