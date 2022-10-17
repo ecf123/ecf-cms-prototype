fbe2f05d87bec5fedeec7c0c690933b9a14ae631
Chen <liangchen_amelia@163.com>
2022-10-17 14:41:25 +0100

    testing finished

--- src/components/Layout/Layout.test.jsx
+++ src/components/Layout/Layout.test.jsx
@@ -1,4 +1,6 @@
+import userEvent from "@testing-library/user-event";
 import { customRender } from "../../utils/testUtils";
+import { screen } from "@testing-library/react";
 import Layout from "./Layout";
 
 it("Should render layout with children", () => {
@@ -10,3 +12,28 @@ it("Should render layout with children", () => {
 
   expect(container).toMatchSnapshot();
 });
+
+it("should render the correct page title when clicking", () => {
+  customRender( <Layout /> )
+
+  const dashBoard = screen.queryAllByText("Dashboard")[0]
+  userEvent.click(dashBoard)
+
+  const pageTitle = screen.getByRole("heading", {level: 1})
+
+  expect(pageTitle.innerHTML).toBe("Dashboard");
+
+})
+
+
+it("when clicking the marketplace, the title should be the same with the choice", () => {
+  customRender( <Layout /> )
+
+  const marketPlace = screen.queryAllByText("Marketplace")[0]
+  userEvent.click(marketPlace)
+
+  const pageTitle = screen.getByRole("heading", {level: 1})
+
+  expect(pageTitle.innerHTML).toBe("Marketplace");
+
+})
