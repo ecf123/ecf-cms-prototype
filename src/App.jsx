2c6748a67b629544d3c5cf4ea984adc58e22b750
atif298 <atifuddin03@gmail.com>
2022-10-17 15:07:32 +0100

    Reverted to normal

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import AddQuestion from "./components/AddQuestion/AddQuestion";
-// import Home from "./pages/Home/Home";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
 
 const App = () => {
-
-  const answer = ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"];
-  const question = "Which Product does Adrian sell the most on Amazon"
-  const questionNumber = 7
-
   return (
     <Routes>
-      <Route path="*" element={<AddQuestion question= {question} answers={answer} questionNumber={questionNumber}/>} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 };
