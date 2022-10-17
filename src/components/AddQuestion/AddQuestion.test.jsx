9efc542b6c3c9673c35e2967dffc520b6ffb2769
atif298 <atifuddin03@gmail.com>
2022-10-17 15:07:48 +0100

    Tests passed and formatted

--- src/components/AddQuestion/AddQuestion.test.jsx
+++ src/components/AddQuestion/AddQuestion.test.jsx
@@ -1,33 +1,60 @@
 import { render, screen } from "@testing-library/react";
-import AddQuestion from "./AddQuestion";
 import { customRender } from "../../utils/testUtils";
+import AddQuestion from "./AddQuestion";
 
-const answers = ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"];
-const question = "What sells on Amazon the most"
-const questionNumber = 7
+const answers = [
+  "Grenade Protein Bars *",
+  "pHD Smart Plan Protein Bar *",
+  "Professional Diva HairDryer",
+  "Black Plus Decker Heater",
+  "Serious Mass Protein Powder *",
+];
+const question = "What sells on Amazon the most";
+const questionNumber = 7;
 
 it("Should match the snapshot, with the props", () => {
   const { container } = customRender(
-    <AddQuestion question= {question} answers={answers} questionNumber={questionNumber}/>
+    <AddQuestion
+      question={question}
+      answers={answers}
+      questionNumber={questionNumber}
+    />
   );
   expect(container).toMatchSnapshot();
 });
 
-
 it("Should render the Questions title", () => {
-  render(<AddQuestion question= {question} answers={answers} questionNumber={questionNumber}/>);
+  render(
+    <AddQuestion
+      question={question}
+      answers={answers}
+      questionNumber={questionNumber}
+    />
+  );
   const title = screen.getByText(/What sells on Amazon the most/);
   expect(title).toBeInTheDocument();
 });
 
 it("Should render the Question answers", () => {
-    render(<AddQuestion question= {question} answers={answers} questionNumber={questionNumber}/>);
-    const listItem = screen.getByRole("list");
-    expect(listItem).toBeInTheDocument();
+  render(
+    <AddQuestion
+      question={question}
+      answers={answers}
+      questionNumber={questionNumber}
+    />
+  );
+  const listItem = screen.getByRole("list");
+  expect(listItem).toBeInTheDocument();
 });
 
 it("Should render the Question buttons svg", () => {
-  render(<AddQuestion question= {question} answers={answers} questionNumber={questionNumber}/>);
+  render(
+    <AddQuestion
+      question={question}
+      answers={answers}
+      questionNumber={questionNumber}
+    />
+  );
   const deleteIcon = screen.getByAltText("Delete Question");
   const editIcon = screen.getByAltText("Edit Question");
   expect(deleteIcon).toBeInTheDocument();
@@ -35,8 +62,13 @@ it("Should render the Question buttons svg", () => {
 });
 
 it("Should render the Question with the correct answer", () => {
-  render(<AddQuestion question= {question} answers={answers} questionNumber={questionNumber}/>);
+  render(
+    <AddQuestion
+      question={question}
+      answers={answers}
+      questionNumber={questionNumber}
+    />
+  );
   const correctAnswer = screen.queryAllByText(/correct/).length;
   expect(correctAnswer).toEqual(3);
-
 });
