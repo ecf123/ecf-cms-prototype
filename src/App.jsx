a8420f70d698ba693a57c37a8ea39ddc0dd4f951
atif298 <atifuddin03@gmail.com>
2022-10-17 15:48:27 +0100

    Mock styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import QuestionContainer from "./containers/QuestionContainer/QuestionContainer";
+//import Home from "./pages/Home/Home";
 import "./styles/main.scss";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<QuestionContainer />} />
     </Routes>
   );
 };
--- src/containers/QuestionContainer/QuestionContainer.jsx
+++ src/containers/QuestionContainer/QuestionContainer.jsx
@@ -1,12 +1,22 @@
-import "QuestionContainer.scss"
+import AddQuestion from "../../components/AddQuestion/AddQuestion"
+import "./QuestionContainer.scss"
 
-const QuestionContainer = () => {
+const QuestionContainer = ({questionsArray}) => {
 
+    const answers = ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"];
+    const question = "What sells on Amazon the most"
+    const questionNumber = 7
 
+    const questionsArrayJSX = questionsArray.map((answer, index) => {
+        return (
+            <AddQuestion question= {question} answers={answers} questionNumber={questionNumber}/>
+            <span></span>
+        )
+        })
 
   return (
     <div className="questions">
-        
+        {questionsArrayJSX}
     </div>
   )
 }
