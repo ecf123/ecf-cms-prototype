642f38718c716bef2046aacf9716e69b38f62d50
atif298 <atifuddin03@gmail.com>
2022-10-19 15:39:39 +0100

    Mock data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+//import Home from "./pages/Home/Home";
+import QuestionContainer from "./containers/QuestionContainer/QuestionContainer";
 import "./styles/main.scss";
 
 const App = () => {
+
+  const questionsArray = [
+    {question:"What sells on Amazon the most", 
+    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
+    questionNumber: 1},
+    {question:"What sells on Amazon the most", 
+    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
+    questionNumber: 1},
+    {question:"What sells on Amazon the most", 
+    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
+    questionNumber: 1},
+    {question:"What sells on Amazon the most", 
+    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
+    questionNumber: 1},
+    {question:"What sells on Amazon the most", 
+    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
+    questionNumber: 1},
+    {question:"What sells on Amazon the most", 
+    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
+    questionNumber: 1}
+  ]
+
   return (
     <Routes>
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<QuestionContainer questionsArray={questionsArray}/>} />
     </Routes>
     
   );
