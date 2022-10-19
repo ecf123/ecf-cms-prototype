f4444ffa97c1627cebc3fc3e70c65cc19bd7e05f
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-10-19 16:55:27 +0100

    changes to styles an code

--- src/containers/AddingLessonQuizContainer/AddingLessonQuizContainer.jsx
+++ src/containers/AddingLessonQuizContainer/AddingLessonQuizContainer.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import "./AddingLessonQuizContainer.scss";
 import Button from "../../components/Button/Button"
+import LoginInputBox from '../../components/LoginInputBox/LoginInputBox';
 
 const AddingLessonQuizContainer = () => {
   return (
     <div className='add-lesson'>
-        <label htmlFor="">Lesson Name</label>
-        <input className='add-lesson__input' type="text" />
+        < LoginInputBox inputBoxStyle={"input-box input-box__label"} />
 
 
         {/* <SelectLesson /> */}
 
 
-        <label htmlFor="">Estimated Completion Time</label>
+        {/* <label htmlFor="">Estimated Completion Time</label>
         <input className='add-lesson__input' type="text" />
+
         <label htmlFor="">Question</label>
-        <input className='add-lesson__input' type="text" />
+        <input className='add-lesson__input' type="text" /> */}
+
         <label htmlFor="">Answers</label>
         <input className='add-lesson__answer' type="text" />
         <input className='add-lesson__answer' type="text" />
@@ -23,21 +25,21 @@ const AddingLessonQuizContainer = () => {
         <input className='add-lesson__answer' type="text" />
         <a className='add-lesson__add' href="">Add +</a>
         <div className="add-lesson__buttons">
-          <div className="add-lesson__cancel">
-            <Button
-              style={"button black round-border large"}
-              textStyle={"text large-text black"}
-              buttonText={"Submit"}
-              // buttonFunction={storeInputs}
-            />
-          </div>
-          <div className="add-lesson__submit">
-            <Button
-              style={"button light-grey round-border large"}
-              textStyle={"text large-text light-grey"}
-              buttonText={"Cancel"}
-            />
-          </div>
+            <div className="add-lesson__submit">
+              <Button
+                style={"button light-grey round-border large"}
+                textStyle={"text large-text light-grey"}
+                buttonText={"Cancel"}
+              />
+            </div>
+            <div className="add-lesson__cancel">
+              <Button
+                style={"button black round-border large"}
+                textStyle={"text large-text black"}
+                buttonText={"Submit"}
+                // buttonFunction={storeInputs}
+              />
+            </div>
         </div>
 
     </div>
