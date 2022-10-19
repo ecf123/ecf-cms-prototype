71c75c3a1292a477089e957efe5ef0a143f29506
michal <michalwagh@gmail.com>
2022-10-19 16:51:17 +0100

    bug fixing in progress

--- src/containers/ListContainer/ListContainer.jsx
+++ src/containers/ListContainer/ListContainer.jsx
@@ -7,7 +7,24 @@ import { ReactComponent as Pencil } from "../../assets/pen-icon.svg";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-const ListContainer = ({ title, cards }) => {
+const ListContainer = ({ title, data }) => {
+
+const cards=data.map(element=>{
+  const paragraphContent=element.articleContent[0].paragraph
+
+
+
+  
+  return{
+    title:element.title,
+    img: element.thumbnail,
+    cardInfo:paragraphContent.substr(0, Math.min(paragraphContent.length, paragraphContent.indexOf(".",50))),
+    dateOrTime: element.date,
+    links: element.category.split(",")
+   
+  }
+})
+
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearchInput = (event) => {
