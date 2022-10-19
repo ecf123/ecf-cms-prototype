0f64055aa0dd9c08a08c13b032b0de172656afe2
atif298 <atifuddin03@gmail.com>
2022-10-19 16:29:41 +0100

    Reset to default App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-//import Home from "./pages/Home/Home";
-import QuestionContainer from "./containers/QuestionContainer/QuestionContainer";
+import Home from "./pages/Home/Home";
 import "./styles/main.scss";
 
 const App = () => {
-
-  const questionsArray = [
-    {question:"What sells on Amazon the most", 
-    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
-    questionNumber: 1},
-    {question:"What sells on Amazon the most", 
-    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
-    questionNumber: 1},
-    {question:"What sells on Amazon the most", 
-    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
-    questionNumber: 1},
-    {question:"What sells on Amazon the most", 
-    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
-    questionNumber: 1},
-    {question:"What sells on Amazon the most", 
-    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
-    questionNumber: 1},
-    {question:"What sells on Amazon the most", 
-    answers: ["Grenade Protein Bars *", "pHD Smart Plan Protein Bar *", "Professional Diva HairDryer", "Black Plus Decker Heater", "Serious Mass Protein Powder *"],
-    questionNumber: 1}
-  ]
-
   return (
     <Routes>
-      <Route path="*" element={<QuestionContainer questionsArray={questionsArray}/>} />
+      <Route path="*" element={<Home />} />
     </Routes>
     
   );
