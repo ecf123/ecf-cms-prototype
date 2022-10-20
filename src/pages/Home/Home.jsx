9cf700b2071da96a146fb87b0829e8b2c4af05a0
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-20 15:03:07 +0100

    clean home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,73 +1,8 @@
 import Layout from "../../components/Layout/Layout";
-import ListContainer from "../../containers/ListContainer/ListContainer";
-import forHire from "../../assets/for-hire-image.svg";
-import manInSuit from "../../assets/man-in-suit.svg";
-import laptop from "../../assets/laptop-with-code.svg";
-
-import profile from "../../assets/profile-picture.svg";
 
 const Home = () => {
-  const user = {
-    image: profile,
-    name: "Brett Zieme",
-    role: "Admin",
-  };
-
-  const cards = [
-    {
-      title: "Want a winning CV? Try these 5 tips",
-      cardInfo:
-        "Et, cras pellentesque eget lectus quis quam porta. Molestie dictumst at at tincidunt.",
-      img: forHire,
-      dateOrTime: "17 Jan 2022",
-      links: ["Professional Development", "Career"],
-    },
-    {
-      title: "5 most in-demand jobs in 2022",
-      cardInfo:
-        "Posuere ultricies nibh malesuada ut nunc sed mollis. Elementum nunc augue lectus malesuada.",
-      img: manInSuit,
-      dateOrTime: "14 Jan 2022",
-      links: ["Jobs", "Career"],
-    },
-    {
-      title: "My journey from a Future Hub user to Web Developer",
-      cardInfo:
-        "Vestibulum ut risus magna est. Dictum at vulputate sodales suspendisse lacinia lectus.",
-      img: laptop,
-      dateOrTime: "22 Dec 2021",
-      links: ["Professional Development", "Programming"],
-    },
-    {
-      title: "Want a winning CV? Try these 5 tips2",
-      cardInfo:
-        "Et, cras pellentesque eget lectus quis quam porta. Molestie dictumst at at tincidunt.",
-      img: forHire,
-      dateOrTime: "17 Jan 2022",
-      links: ["Professional Development", "Career"],
-    },
-    {
-      title: "5 most in-demand jobs in 2022x",
-      cardInfo:
-        "Posuere ultricies nibh malesuada ut nunc sed mollis. Elementum nunc augue lectus malesuada.",
-      img: manInSuit,
-      dateOrTime: "14 Jan 2022",
-      links: ["Jobs", "Career"],
-    },
-    {
-      title: "My journey from a Future Hub user to Web Developer2",
-      cardInfo:
-        "Vestibulum ut risus magna est. Dictum at vulputate sodales suspendisse lacinia lectus.",
-      img: laptop,
-      dateOrTime: "22 Dec 2021",
-      links: ["Professional Development", "Programming"],
-    },
-  ];
-
   return (
-    <Layout user={user}>
-      <ListContainer title="Article" cards={cards} />
-    </Layout>
+    <Layout></Layout>
   );
 };
 
