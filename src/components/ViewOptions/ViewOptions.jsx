698a2e5ceafb425c987d00e795e1a4e97a6ee8d0
DorBia <dorota.bialy95@gmail.com>
2022-10-20 17:05:43 +0100

    Fixed bug

--- src/components/ViewOptions/ViewOptions.jsx
+++ src/components/ViewOptions/ViewOptions.jsx
@@ -4,19 +4,20 @@ import { Link } from "react-router-dom";
 
 const ViewOptions = ({ selectedOption }) => {
   const [selected, setSelected] = useState(selectedOption); 
- const selectionChange = (option)=> {
-  setSelected(option)
- }
+
+ const selectionChange = (option) => setSelected(option);
+
+ const setClassName = (option) => selected != option ? "view-options__overview" : "view-options__overview--clicked";
 
   return (
     <div className="view-options">
-      <Link to ="*" onClick={() => selectionChange("Overview")} className={!selected?"view-options__overview":"view-options__overview--clicked"} >
+      <Link to ="*" onClick={() => selectionChange("Overview")} className={setClassName("Overview")} >
         Overview
       </Link>
-      <Link to ="*" onClick={() => selectionChange("Courses")} className={!selected?"view-options__courses":"view-options__courses--clicked"} >
+      <Link to ="*" onClick={() => selectionChange("Courses")} className={setClassName("Courses")} >
         Courses
       </Link>
-      <Link to ="*" onClick={() => selectionChange("Stats")} className={!selected?"view-options__stats":"view-options__stats--clicked"} >
+      <Link to ="*" onClick={() => selectionChange("Stats")} className={setClassName("Stats")} >
         Stats
       </Link>
     </div>
