9724fd27dce574b093800e34f92205608a4144e9
AdrianRotila <adrianrotila@gmail.com>
2022-10-20 15:47:23 +0100

    Styles changed

--- src/components/AddLessonAdditionalBoxes/AddLessonAdditionalBoxes.jsx
+++ src/components/AddLessonAdditionalBoxes/AddLessonAdditionalBoxes.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import "./AddLessonAdditionalBoxes.scss";
+import FreeType from '../FreeType/FreeType';
+import Short from '../Short/Short';
 
-const AddLessonAdditionalBoxes = () => {
+const AddLessonAdditionalBoxes = ({handleFreeTypeValue, handleShortValue, handleAddLessonContent}) => {
   return (
     <div className='add-lesson'>
-        <label className='add-lesson__label-one' htmlFor="">Additional Subtitle(s) (optional)</label>
-        <input className='add-lesson__input-one input' name="input-one" type="text" placeholder='e.g. Fund Manager'/>
-        <label className='add-lesson__label-two' htmlFor="">Addtional Content (optional)</label>
-        <input className='add-lesson__input-two input' name="input-two" type="text" />
-        <h5 className='add-lesson__add'>Add +</h5>
+        <Short 
+          shortLabelText = "Additional Subtitle(s) (optional)"
+          shortType = "text"
+          shortPlaceHolderText = "e.g. Fund Manager"
+          handleShortValue = {handleShortValue}
+          inputClassName = "add-lesson__input-one input"
+          name = "input-one"/>
+        <FreeType 
+          freeTypeLabelText = "Additional Content (optional)"
+          handleFreeTypeValue = {handleFreeTypeValue}
+          name = "input-two"
+          inputClassName = 'add-lesson__input-two input' />
+        <h5 className='add-lesson__add' onClick={handleAddLessonContent}>Add +</h5>
     </div>
   )
 }
