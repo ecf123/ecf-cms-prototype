0d3c9806fafc5e6cc4184233caf20736b0034d53
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-10-20 15:20:04 +0100

    added style to the page

--- src/containers/PathwaysListContainer/PathwaysListContainer.jsx
+++ src/containers/PathwaysListContainer/PathwaysListContainer.jsx
@@ -3,36 +3,66 @@ import ListComponentUniversal from "../../components/ListComponentUniversal/List
 import { Modal, Input } from "antd";
 import { useState } from "react";
 import PageTitle from "../../components/PageTitle/PageTitle";
-import ViewOptions from "../../components/ViewOptions/ViewOptions";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Button from "../../components/Button/Button";
+import { Link } from "react-router-dom";
+import {ReactComponent as Pencil} from "../../assets/pen-icon.svg";
 
-const entryListContainer = ({ dataJSON }) => {
+const dataJSON = [];
+for (let i = 0; i < 6; i++) {
+dataJSON.push({
+ key: i,
+ id: i,
+ pathway_name: `Financial Forest + ${i}`,
+ providers: [
+  "https://firebasestorage.googleapis.com/v0/b/ecf-future-hub.appspot.com/o/pathway%2Ficons%2Fhsbc.svg?alt=media&token=f568f1fc-ade3-4bbf-9aee-1dc841cce600",
+  "https://firebasestorage.googleapis.com/v0/b/ecf-future-hub.appspot.com/o/pathway%2Ficons%2Fhalifax.svg?alt=media&token=0b431ade-cb19-4cc1-bc7d-b4d4456146a0",
+ //  Barclays,
+ //  Airbnb,
+ ],
+ no_of_courses: `8`,
+ no_of_lessons: `8`,
+ est_completion_time: `20hrs`,
+ no_enrolled: 32,
+ category: [
+  "design",
+  "business",
+  "digital marketing",
+  "programming",
+  "finance",
+ ],
+});
+}
+
+const PathwaysListContainer = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [editingEntry, setEditingEntry] = useState(null);
-  const [dataSource, setDataSource] = useState(
-    dataJSON
-      ? dataJSON.map((entry) => {
-          let providersArr = [
-            entry.skillstree.children[0].icon,
-            entry.skillstree.children[0].children[1].children[0].icon,
-          ];
-          entry.skillstree.children[0].children.forEach((part) => {
-            providersArr.push(part.icon);
-          });
+  const [dataSource, setDataSource] = useState(dataJSON);
+    // dataJSON
+    //   ? dataJSON.map((entry) => {
+    //       let providersArr = [
+    //         entry.skillstree.children[0].icon,
+    //         entry.skillstree.children[0].children[1].children[0].icon,
+    //       ];
+    //       entry.skillstree.children[0].children.forEach((part) => {
+    //         providersArr.push(part.icon);
+    //       });
 
-          return {
-            key: entry.id,
-            id: entry.id,
-            pathway_name: entry.header,
-            providers: providersArr,
-            no_of_courses: entry.skillstree.children[0].children.length,
-            no_of_lessons: Math.floor(Math.random() * 150 + 20),
-            entry_length: Math.floor(Math.random() * 25 + 5) + " hrs",
-            // missing name from database
-            students_enrolled: Math.floor(Math.random() * 15 + 10),
-          };
-        })
-      : dataJSON
-  );
+  //         return {
+  //           key: entry.id,
+  //           id: entry.id,
+  //           pathway_name: entry.header,
+  //           providers: providersArr,
+  //           no_of_courses: entry.skillstree.children[0].children.length,
+  //           no_of_lessons: Math.floor(Math.random() * 150 + 20),
+  //           entry_length: Math.floor(Math.random() * 25 + 5) + " hrs",
+  //           // missing name from database
+  //           students_enrolled: Math.floor(Math.random() * 15 + 10),
+  //         };
+  //       })
+  //     : dataJSON
+  // );
+
 
   const onDelete = (e) => {
     Modal.confirm({
@@ -61,10 +91,26 @@ const entryListContainer = ({ dataJSON }) => {
       <div className="entry-list__header">
         <PageTitle
           className="entry-list__title page-title"
-          title="entry List"
+          title="Pathways List"
         />
-        <ViewOptions selectedOption="entrys" />
       </div>
+
+      <nav className="entry-list__nav">
+          <SearchBar
+            placeHolderText={"Search for placeholder"}
+          />
+          <Link to="*">
+            <Button
+              style={"button black round-border large"}
+              imgStyle={"medium-img white-svg"}
+              textStyle={"text medium-text black"}
+              buttonText={"Add Placeholder"}
+            >
+              <Pencil />
+            </Button>
+          </Link>
+        </nav>
+
       <div className="entry-list__table">
         <ListComponentUniversal
           onDelete={onDelete}
@@ -113,4 +159,4 @@ const entryListContainer = ({ dataJSON }) => {
   );
 };
 
-export default entryListContainer;
+export default PathwaysListContainer;
