6191746e741da5c2b04a9feebae183cefc046297
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-10-20 15:07:42 +0100

    changes to styles

--- src/components/AddLessonAdditionalBoxes/AddLessonAdditionalBoxes.jsx
+++ src/components/AddLessonAdditionalBoxes/AddLessonAdditionalBoxes.jsx
@@ -4,9 +4,9 @@ import "./AddLessonAdditionalBoxes.scss";
 const AddLessonAdditionalBoxes = () => {
   return (
     <div className='add-lesson'>
-        <label className='add-lesson__label' htmlFor=""></label>
+        <label className='add-lesson__label-one' htmlFor="">Additional Subtitle(s) (optional)</label>
         <input className='add-lesson__input-one input' name="input-one" type="text" placeholder='e.g. Fund Manager'/>
-        <label className='add-lesson__label' htmlFor=""></label>
+        <label className='add-lesson__label-two' htmlFor="">Addtional Content (optional)</label>
         <input className='add-lesson__input-two input' name="input-two" type="text" />
         <h5 className='add-lesson__add'>Add +</h5>
     </div>
