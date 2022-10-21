87231361711086201f24897fd563c636c0ddf9b0
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-21 11:54:21 +0100

    rendered in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,10 @@ import Layout from "../../components/Layout/Layout";
 import CourseOverview from "../../containers/CourseOverview/CourseOverview";
 import HeaderContainer from "../../containers/HeaderContainer/HeaderContainer";
 const Home = () => {
-  return <Layout>
-<<<<<<< HEAD
+  return <Layout> 
     <HeaderContainer
   titleLarge={"financial forest"}/> 
     <CourseOverview />
-=======
->>>>>>> 4da0eacbaa5f61976e369bf8d5751905f2fbec0e
   </Layout>;
 };
 export default Home;
