f37efd3ab8237bce85acd087a3912a6f9cbb44ea
Amelia Baldwin <amelia01000010@gmail.com>
2022-10-21 10:40:21 +0100

    pages created

---
+++ src/pages/Articles/Articles.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListContainer from '../../containers/ListContainer/ListContainer'
+
+const Articles = () => {
+  return (
+   <ListContainer title="Articles"/>
+  )
+}
+
+export default Articles
---
+++ src/pages/Pathways/Pathways.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Pathways = () => {
+  return (
+
+  <></>)
+}
+
+export default Pathways
