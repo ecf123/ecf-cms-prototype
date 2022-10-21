e210a2dd05d23dafcbaefc7901240c11ed500082
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-21 14:17:29 +0100

    styling update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,7 @@
 import Layout from "../../components/Layout/Layout";
 import CourseOverview from "../../containers/CourseOverview/CourseOverview";
-import HeaderContainer from "../../containers/HeaderContainer/HeaderContainer";
 const Home = () => {
   return <Layout> 
-    <HeaderContainer
-  titleLarge={"financial forest"}/> 
     <CourseOverview />
   </Layout>;
 };
