3a5afa253ae37b6d325f31db12b8a4a0395801af
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-10-21 11:17:15 +0100

    pulled main

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import Layout from "../../components/Layout/Layout";
+import QuestionAnswer from "../../components/QuestionAnswer/QuestionAnswer";
 const Home = () => {
   return <Layout>
+    <QuestionAnswer />
   </Layout>;
 };
 export default Home;
