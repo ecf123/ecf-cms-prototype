e6330979bb4221b097b8bc10136af1c632d743f6
AdrianRotila <adrianrotila@gmail.com>
2022-10-21 14:42:09 +0100

    added media upload

--- src/containers/AddLessonContainer/AddLessonContainer.jsx
+++ src/containers/AddLessonContainer/AddLessonContainer.jsx
@@ -1,48 +1,82 @@
-import React from "react";
 import "./AddLessonContainer.scss";
+import { useState } from "react";
 import Short from "../../components/Short/Short";
 import SelectComponent from "../../components/SelectComponent/SelectComponent";
 import Button from "../../components/Button/Button";
+import MediaUploadBox from "../../components/MediaUploadBox/MediaUploadBox"
 
-const AddLessonContainer = ({ handleShortValue, storeInputs}) => {
+const AddLessonContainer = ({ handleShortValue, handleSubmit, handleCancel}) => {
+  
+  const [pageInfo, setPageInfo] = useState({
+    file: null,
+    fileName: "",
+    shortInputOne: "",
+    shortInputTwo: "",
+    freeTypeInputOne: "",
+    freeTypeInputTwo: "",
+  });  
+  
+  const handleFileChange = (e) => {
+    if (!pageInfo.file) setPageInfo({ fileName: e.target.files[0].name });
+    pageInfo.file
+      ? setPageInfo({ ...pageInfo, file: null })
+      : setPageInfo({
+          ...pageInfo,
+          file: URL.createObjectURL(e.target.files[0]),
+        });
+  };
+
   return (
-    <div className="add-lesson__container">
-      <Short
-        shortLabelText="Lesson Name"
-        shortType="text"
-        //   shortPlaceHolderText,
-        handleShortValue={handleShortValue}
-        inputClassName="short__input"
-        name="lesson name"
-      />
-      <SelectComponent className="add-lesson__select" />
-      <Short
-        shortLabelText="Lesson Name"
-        shortType="text"
-        //   shortPlaceHolderText,
-        handleShortValue={handleShortValue}
-        inputClassName="short__input"
-        name="lesson name"
-      />
+    <div className="add-lesson">
+      <div className="add-lesson__container">
+        <Short
+          shortLabelText="Lesson Name"
+          shortType="text"
+          // No placeholder needed
+          handleShortValue={handleShortValue}
+          inputClassName="short__input"
+          name="lesson name"
+        />
+        <SelectComponent className="add-lesson__select" />
+        <Short
+          shortLabelText="Estimated Completion Time"
+          shortType="text"
+          // No placeholder needed
+          handleShortValue={handleShortValue}
+          inputClassName="short__input"
+          name="estimated completion time"
+        />
 
-        <h5 className='add-lesson__add'>Add +</h5>
 
-      <div className="add-lesson__buttons">
-      <div className="add-lesson__submit">
-          <Button
-            style={"button light-grey round-border large"}
-            textStyle={"text large-text light-grey"}
-            buttonText={"Cancel"}
-          />
+          <h5 className='add-lesson__add'>Add +</h5>
+
+
+        <div className="add-lesson__buttons">
+            <Button
+              style={"button light-grey round-border large"}
+              textStyle={"text large-text light-grey"}
+              buttonText={"Cancel"}
+              buttonFunction={handleCancel}
+            />
+            <Button
+              style={"button black round-border large"}
+              textStyle={"text large-text black"}
+              buttonText={"Submit"}
+              buttonFunction={handleSubmit}
+            />
         </div>
-        <div className="add-lesson__cancel">
-          <Button
-            style={"button black round-border large"}
-            textStyle={"text large-text black"}
-            buttonText={"Submit"}
-            buttonFunction={storeInputs}
+      </div>
+      <div className="add-lesson__upload">
+        <MediaUploadBox 
+          file={pageInfo.file}
+          handleFileChange={handleFileChange}
+          uploadLabelName = "Lesson Upload"
+          uploadButtonText = "Media Upload"
+          fileName={pageInfo.fileName}
           />
-        </div>
+      </div>
+      <div className="add-lesson__content">
+        
       </div>
     </div>
   );
