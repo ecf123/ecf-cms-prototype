a767c702246422d186e1661fc535d2b3f53229f2
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-21 10:49:26 +0100

    trying different styling

--- src/containers/CourseOverview/CourseOverview.jsx
+++ src/containers/CourseOverview/CourseOverview.jsx
@@ -4,21 +4,25 @@ import MoreOptions from '../../components/MoreOptions/MoreOptions'
 import PathwayIntroduction from '../../components/PathwayIntroduction/PathwayIntroduction'
 import PotentialCareers from '../../components/PotentialCareers/PotentialCareers'
 import SalaryInfo from '../../components/SalaryInfo/SalaryInfo'
+import PathwayOverviewHeader from '../../components/PathwayOverviewHeader/PathwayOverviewHeader'
+import handleDelete from '../../components/MoreOptions/MoreOptions'
+import handleEdit from "../../components/MoreOptions/MoreOptions"
+import './CourseOverview.scss'
 
 const CourseOverview = () => {
   return (
-    <>
+    <section className='course-overview'>
         <div className="course-overview__main">
-        <PathwayIntroduction image={image} description={"Lorem Ipsum"}/>
-        {/* add in pathway component here */}
-        <DropDown topicTitle={"Financial Forest"} lessonArr={lessonArr}/>
+        <PathwayIntroduction className="pi" image={"image"} description={"Lorem Ipsum"}/>
+        <PathwayOverviewHeader className="course-overview__main--poh"link={"link"}/>
+        <DropDown className="course-overview__main--dd"topicTitle={"Financial Forest"} lessonArr={["english", "maths", "physics"]}/>
         </div>
         <div className='course-overview__side'>
-        <MoreOptions title="Financal Forest" description="Lorem ipsum" handleDelete={handleDelete} handleEdit={handleEdit}/>
-        <SalaryInfo salariesObj={salariesObj}/>
-        <PotentialCareers potentialCareers={PotentialCareers}/>
+        <MoreOptions className='course-overview__side--mo'title="Financal Forest" description="Lorem ipsum" handleDelete={handleDelete} handleEdit={handleEdit}/>
+        <SalaryInfo className='course-overview__side--si'salariesObj={[1, 2, 3]}/>
+        <PotentialCareers className='course-overview__side--pc'potentialCareers={["doctor", "nurse", "developer"]}/>
         </div>
-    </>
+    </section>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,9 @@
 import Layout from "../../components/Layout/Layout";
-<<<<<<< HEAD
 import CourseOverview from "../../containers/CourseOverview/CourseOverview";
-
 const Home = () => {
-  return (
-    <Layout>
-      <CourseOverview />
-    </Layout>
-  );
-=======
-const Home = () => {
-  return <Layout></Layout>;
->>>>>>> 78245a321d545888e1a1917f7a5d5e185cba8fe7
+  return <Layout>
+    <CourseOverview />
+  </Layout>;
 };
 
 export default Home;
