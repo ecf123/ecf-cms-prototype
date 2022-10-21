c19694393fb386c3368abefd3109545f966ee106
lucy gilhooly <lucy_johannah@hotmail.com>
2022-10-21 11:04:00 +0100

    stying

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
 import Layout from "../../components/Layout/Layout";
 import CourseOverview from "../../containers/CourseOverview/CourseOverview";
+import HeaderContainer from "../../containers/HeaderContainer/HeaderContainer";
 const Home = () => {
   return <Layout>
+    <HeaderContainer
+  titleLarge={"financial forest"}/> 
     <CourseOverview />
   </Layout>;
 };
