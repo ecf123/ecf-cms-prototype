05f186a1105a9a2bb652f9c3eb6f3a03fbe8968d
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-21 11:56:58 +0100

    npm run format

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,56 @@
 import "./styles/main.scss";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Layout  from "./components/Layout/Layout";
-import profile from "./assets/profile-picture.svg"
+import Layout from "./components/Layout/Layout";
+import profile from "./assets/profile-picture.svg";
 
 const App = () => {
-
   const user = {
     image: profile,
     name: "Brett Zieme",
     role: "Admin",
   };
- 
+
   return (
     <>
-    {/* loggedOut && 
+      {/* loggedOut && 
     <Routes>
       <Route />
     </Routes> */}
-    {
-    <Layout user={user}>
-      <Routes>
-        <Route path="/" element={<Home/>} /> 
+      {
+        <Layout user={user}>
+          <Routes>
+            <Route path="/" element={<Home />} />
 
-        <Route path="/pathways" element={<p>Pathways</p>}/>
-        <Route path="/pathways/:pathwayName" element={<p>Pathway name</p>}/>
-        <Route path="/:pathwayName/courses" element={<p>Courses</p>}/>
-        <Route path="/:pathwayName/courses/list" element={<p>Courses list</p>}/>
-        <Route path="/:courseName" element={<p>Course name</p>}/>
-        <Route path="/:courseName/addlesson" element={<p>Add lesson</p>}/>
-        <Route path="/:courseName/:lessonTitle" element={<p>Lesson title</p>}/>
+            <Route path="/pathways" element={<p>Pathways</p>} />
+            <Route path="/:pathwayName" element={<p>Pathway name</p>} />
+            <Route path="/:pathwayName/courses" element={<p>Courses</p>} />
+            <Route
+              path="/:pathwayName/courses/list"
+              element={<p>Courses list</p>}
+            />
+            <Route path="/:courseName" element={<p>Course name</p>} />
+            <Route path="/:courseName/addlesson" element={<p>Add lesson</p>} />
+            <Route
+              path="/:courseName/:lessonTitle"
+              element={<p>Lesson title</p>}
+            />
 
-        <Route path="/articles" element={<p>Articles</p>}/>
-        <Route path="/:articleTitle" element={<p>Article title</p>}/>
-        <Route path="/addarticle" element={<p>Add article</p>}/>
+            <Route path="/articles" element={<p>Articles</p>} />
+            <Route path="/:articleTitle" element={<p>Article title</p>} />
+            <Route path="/addarticle" element={<p>Add article</p>} />
 
-        <Route path="/marketplace" element={<p>Marketplace</p>}/>
-        <Route path="/addprize" element={<p>Add prize</p>}/>
-        <Route path="/:prizeName" element={<p>Prize name</p>}/>
+            <Route path="/marketplace" element={<p>Marketplace</p>} />
+            <Route path="/addprize" element={<p>Add prize</p>} />
+            <Route path="/:prizeName" element={<p>Prize name</p>} />
 
-        <Route path="/users" element={<p>Users</p>}/>
-        <Route path="/settings" element={<p>Settings</p>}/>
-        
-
-      </Routes>
-    </Layout>
-   /* && loggedIn */}
-   </>
+            <Route path="/users" element={<p>Users</p>} />
+            <Route path="/settings" element={<p>Settings</p>} />
+          </Routes>
+        </Layout>
+        /* && loggedIn */
+      }
+    </>
   );
 };
 
--- src/containers/Routing/Routing.jsx
+++ src/containers/Routing/Routing.jsx
@@ -1,49 +1,110 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "../../components/Layout/Layout";
 import ListContainer from "../ListContainer/ListContainer";
-import Login from "../../components/Login/Login"
-import AddPage from "../../containers/AddPage/AddPage"
+import Login from "../../components/Login/Login";
+import AddPage from "../../containers/AddPage/AddPage";
 
 const Routing = () => {
   return (
     <Router>
       <div className="app">
-        {<Routes>
-          <Route path="/notin" element={<Login />}>
-            {/* Not logged in */}
-          </Route>
-        </Routes> /* && !loggedIn  */}
-        {<Layout>
+        {
           <Routes>
-            {/* Logged in */}
-            
-            <Route path="/pathways" element={<p>Pathways</p>}/>
-            <Route path="/pathways/add" element={<AddPage shortLabelTextOne="" shortLabelTextTwo="" shortPlaceHolderTextOne="" shortPlaceHolderTextTwo="" freeTypeLabelTextOne="" freeTypeLabelTextTwo="" uploadLabelName="" />} />
-            <Route path="/pathways/:pathwayId"/>
-            <Route path="/pathways/:pathwayId/courses"/>
-            <Route path="/pathways/:pathwayId/:courseId"/>
-            <Route path="/pathways/:pathwayId/:courseId/add" element={<AddPage shortLabelTextOne="" shortLabelTextTwo="" shortPlaceHolderTextOne="" shortPlaceHolderTextTwo="" freeTypeLabelTextOne="" freeTypeLabelTextTwo="" uploadLabelName="" />} />
-            <Route path="/pathways/:pathwayId/:courseId/:lessonId"/>
+            <Route path="/notin" element={<Login />}>
+              {/* Not logged in */}
+            </Route>
+          </Routes> /* && !loggedIn  */
+        }
+        {
+          <Layout>
+            <Routes>
+              {/* Logged in */}
 
-            <Route path="/articles" element={<ListContainer title="Article"/>} />
-            <Route path="/articles/add" element={<AddPage shortLabelTextOne="" shortLabelTextTwo="" shortPlaceHolderTextOne="" shortPlaceHolderTextTwo="" freeTypeLabelTextOne="" freeTypeLabelTextTwo="" uploadLabelName="" />} />
-            <Route path="/articles/:articleId"/>
+              <Route path="/pathways" element={<p>Pathways</p>} />
+              <Route
+                path="/pathways/add"
+                element={
+                  <AddPage
+                    shortLabelTextOne=""
+                    shortLabelTextTwo=""
+                    shortPlaceHolderTextOne=""
+                    shortPlaceHolderTextTwo=""
+                    freeTypeLabelTextOne=""
+                    freeTypeLabelTextTwo=""
+                    uploadLabelName=""
+                  />
+                }
+              />
+              <Route path="/pathways/:pathwayId" />
+              <Route path="/pathways/:pathwayId/courses" />
+              <Route path="/pathways/:pathwayId/:courseId" />
+              <Route
+                path="/pathways/:pathwayId/:courseId/add"
+                element={
+                  <AddPage
+                    shortLabelTextOne=""
+                    shortLabelTextTwo=""
+                    shortPlaceHolderTextOne=""
+                    shortPlaceHolderTextTwo=""
+                    freeTypeLabelTextOne=""
+                    freeTypeLabelTextTwo=""
+                    uploadLabelName=""
+                  />
+                }
+              />
+              <Route path="/pathways/:pathwayId/:courseId/:lessonId" />
 
+              <Route
+                path="/articles"
+                element={<ListContainer title="Article" />}
+              />
+              <Route
+                path="/articles/add"
+                element={
+                  <AddPage
+                    shortLabelTextOne=""
+                    shortLabelTextTwo=""
+                    shortPlaceHolderTextOne=""
+                    shortPlaceHolderTextTwo=""
+                    freeTypeLabelTextOne=""
+                    freeTypeLabelTextTwo=""
+                    uploadLabelName=""
+                  />
+                }
+              />
+              <Route path="/articles/:articleId" />
 
-            <Route path="/marketplace" element={<ListContainer title="Marketplace"/>}/>
-            <Route path="/marketplace/add" element={<AddPage shortLabelTextOne="" shortLabelTextTwo="" shortPlaceHolderTextOne="" shortPlaceHolderTextTwo="" freeTypeLabelTextOne="" freeTypeLabelTextTwo="" uploadLabelName="" />} />
-            <Route path="/marketplace/:marketplaceId"/>
+              <Route
+                path="/marketplace"
+                element={<ListContainer title="Marketplace" />}
+              />
+              <Route
+                path="/marketplace/add"
+                element={
+                  <AddPage
+                    shortLabelTextOne=""
+                    shortLabelTextTwo=""
+                    shortPlaceHolderTextOne=""
+                    shortPlaceHolderTextTwo=""
+                    freeTypeLabelTextOne=""
+                    freeTypeLabelTextTwo=""
+                    uploadLabelName=""
+                  />
+                }
+              />
+              <Route path="/marketplace/:marketplaceId" />
 
-            <Route path="/users" element={<p>Users</p>}/>
+              <Route path="/users" element={<p>Users</p>} />
 
-            <Route path="/settings" element={<p>Settings</p>}/>
-            
-            <Route path="/" element={<p>Dashboard</p>} />
-          </Routes>
-        </Layout> /* && loggedIn */}
+              <Route path="/settings" element={<p>Settings</p>} />
+
+              <Route path="/" element={<p>Dashboard</p>} />
+            </Routes>
+          </Layout> /* && loggedIn */
+        }
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default Routing
+export default Routing;
