65f7692d349234eecaf4e3acf4d02b3ce6e88713
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-21 10:05:21 +0100

    revert app and index

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import "./styles/main.scss";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import "./styles/main.scss";
 
 const App = () => {
 
  
   return (
-    <Routing />
+    <Routes>
+      <Route path="*" element={<Home/>} />
+    </Routes>
   );
 };
 
