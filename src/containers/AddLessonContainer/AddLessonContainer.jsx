f9f673383482ca42ae8852d7b0c7ccaeff617d45
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-10-21 12:32:09 +0100

    added changes to styles adn layout

--- src/containers/AddLessonContainer/AddLessonContainer.jsx
+++ src/containers/AddLessonContainer/AddLessonContainer.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./AddLessonContainer.scss";
 import Short from "../../components/Short/Short";
 import SelectComponent from "../../components/SelectComponent/SelectComponent";
+import Button from "../../components/Button/Button";
 
-const AddLessonContainer = ({ handleShortValue }) => {
+const AddLessonContainer = ({ handleShortValue, storeInputs}) => {
   return (
-    <div>
+    <div className="add-lesson__container">
       <Short
         shortLabelText="Lesson Name"
         shortType="text"
@@ -13,15 +14,36 @@ const AddLessonContainer = ({ handleShortValue }) => {
         handleShortValue={handleShortValue}
         inputClassName="short__input"
         name="lesson name"
-        />
-        <SelectComponent />
-        <Short
+      />
+      <SelectComponent className="add-lesson__select" />
+      <Short
         shortLabelText="Lesson Name"
         shortType="text"
         //   shortPlaceHolderText,
         handleShortValue={handleShortValue}
         inputClassName="short__input"
-        name="lesson name" />
+        name="lesson name"
+      />
+
+        <h5 className='add-lesson__add'>Add +</h5>
+
+      <div className="add-lesson__buttons">
+      <div className="add-lesson__submit">
+          <Button
+            style={"button light-grey round-border large"}
+            textStyle={"text large-text light-grey"}
+            buttonText={"Cancel"}
+          />
+        </div>
+        <div className="add-lesson__cancel">
+          <Button
+            style={"button black round-border large"}
+            textStyle={"text large-text black"}
+            buttonText={"Submit"}
+            buttonFunction={storeInputs}
+          />
+        </div>
+      </div>
     </div>
   );
 };
