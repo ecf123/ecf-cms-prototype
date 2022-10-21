91e5b2fb9a1813af5db7858caa65ba2c7bd12901
Chen <liangchen_amelia@163.com>
2022-10-21 12:03:10 +0100

    delete the add button and finish testing

--- src/components/QuestionAnswer/QuestionAnswer.jsx
+++ src/components/QuestionAnswer/QuestionAnswer.jsx
@@ -1,19 +1,19 @@
 import "./QuestionAnswer.scss"
 import Short from '../Short/Short'
 
-const QuestionAnswer = ({submitQuestion}) => {
+const QuestionAnswer = ({handleShortValue, onSubmit}) => {
 
 
   return (
-    <form className='form' onSubmit={submitQuestion}>
-        <Short shortLabelText={"Question"}/>
+    
+    <form className='form' onSubmit = {onSubmit}>
+        <Short shortLabelText={"Question"} handleShortValue={handleShortValue}/>
         <p className="form__answers__heading">Answers</p>
         <div className="form__answers">
         <Short />
         <Short />
         <Short />
         <Short />
-        <button className="form__button" >Add +</button>
         </div>
     </form>
   )
