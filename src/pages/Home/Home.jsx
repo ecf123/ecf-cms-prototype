e28b22809e9365c623dc8512b4adf8a253ba9f44
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-10-21 15:17:30 +0100

    removed from home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,7 @@
 import Layout from "../../components/Layout/Layout";
-import QuestionAnswer from "../../components/QuestionAnswer/QuestionAnswer";
 const Home = () => {
   return (
     <Layout>
-      <QuestionAnswer />
     </Layout>
   );
 };
