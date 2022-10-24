1f3d90aeba1e02962cca212946c62dfa0313ca02
AndyBowerman <94122682+AndyBowerman@users.noreply.github.com>
2022-10-24 17:17:44 +0100

    Created registration form with styling and testing (#94)
    * Created registration form with styling and testing
    
    * corrected login tests

---
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,20 @@
+import LoginInputBox from '../LoginInputBox/LoginInputBox';
+import './RegistrationForm.scss';
+
+const RegistrationForm = () => {
+  return (
+    <div className='registration-form'>
+        <h2 className='registration-form__header'>Register</h2>
+        <form className='registration-form__form'>
+            <LoginInputBox labelText="First Name" inputType="text" labelId="first-name"/>
+            <LoginInputBox labelText="Last Name" inputType="text" labelId="last-name"/>
+            <LoginInputBox labelText="Email" inputType="email" labelId="email"/>
+            <LoginInputBox labelText="Password" inputType="password" labelId="password"/>
+            <LoginInputBox labelText="Re-Type Password" inputType="password" labelId="re-type-password"/>
+            <button className="registration-form__button">Create Account</button>
+        </form>
+    </div>
+  )
+}
+
+export default RegistrationForm
---
+++ src/components/RegistrationForm/RegistrationForm.test.jsx
@@ -0,0 +1,31 @@
+import { customRender } from "../../utils/testUtils";
+import RegistrationForm from "./RegistrationForm";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+it("it should match the snapshot", () => {
+    const { container } = customRender(
+      <RegistrationForm />
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it("Should render labels", () => {
+    render(<RegistrationForm />);
+
+    const labelOne = screen.getByText("First Name");
+    const labelTwo = screen.getByText("Last Name");
+    
+    expect(labelOne).toBeInTheDocument();
+    expect(labelTwo).toBeInTheDocument();
+  });
+
+  it("should not display text when password entered", () => {
+    render(<RegistrationForm />);
+  
+    const passwordInput = screen.getByLabelText("Password");
+    userEvent.type(passwordInput, "fix");
+  
+    const password = screen.queryByText("fix");
+    expect(password).toBeFalsy();
+  });
