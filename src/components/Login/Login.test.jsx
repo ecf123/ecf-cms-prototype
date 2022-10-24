da172a1c5d16b22dc73db67248053211c0c830b9
DorBia <95347115+DorBia@users.noreply.github.com>
2022-10-24 17:29:04 +0100

    Fixed merge conflicts

--- src/components/Login/Login.test.jsx
+++ src/components/Login/Login.test.jsx
@@ -1,6 +1,5 @@
-import { screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Login from "./Login";
-import userEvent from "@testing-library/user-event";
 import { customRender } from "../../utils/testUtils";
 
 it("should render the form", () => {
@@ -9,36 +8,19 @@ it("should render the form", () => {
 });
 
 it("should not render the error message on load", () => {
-  customRender(<Login />);
+  render(<Login />);
   const errorMessage = screen.queryByText("Sorry something went wrong");
 
   expect(errorMessage).toBeFalsy();
 });
 
 it("shouldn't render the success message on load", () => {
-  customRender(<Login />);
+  render(<Login />);
   const successMessage = screen.queryByText(
     "Thank you for submitting! We will be in touch"
   );
   expect(successMessage).toBeFalsy();
 });
 
-it("should not submit the form when not all credentials are valid and display error message", () => {
-  customRender(<Login />);
-
-  const passwordInput = screen.getByLabelText("Password");
-  userEvent.type(passwordInput, "fix");
-
-  const button = screen.queryByText("Login");
-  userEvent.click(button);
-
-  const errorMessage = screen.getByText("Sorry something went wrong");
-  const successMessage = screen.queryByText(
-    "Thank you for submitting! We will be in touch"
-  );
-
-  expect(errorMessage).toBeTruthy();
-  expect(successMessage).toBeFalsy();
-});
 
 
