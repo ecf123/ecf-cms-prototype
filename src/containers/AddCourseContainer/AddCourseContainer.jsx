911f02cdd7c4b3377ee46cb3903d8f0b46c2f336
Afaq Munawar <afaqmunawar12@gmail.com>
2022-10-24 14:38:43 +0100

    add course completed and tested

--- src/containers/AddCourseContainer/AddCourseContainer.jsx
+++ src/containers/AddCourseContainer/AddCourseContainer.jsx
@@ -1,25 +1,13 @@
 import "./AddCourseContainer.scss";
 import { useState } from "react";
-import Short from "../../components/Short/Short";
-import FreeType from "../../components/FreeType/FreeType";
-import Button from "../../components/Button/Button";
-import MediaUploadBox from "../../components/MediaUploadBox/MediaUploadBox";
+import AddCourse from "../../components/AddCourse/AddCourse";
 
-const AddCourseContainer = ({
-  shortLabelTextOne,
-  shortLabelTextTwo,
-  shortLabelTextThree,
-  shortPlaceHolderTextTwo,
-  shortPlaceHolderTextThree,
-  freeTypeLabelTextOne,
-  uploadLabelName,
-  uploadLabelNameTwo,
-  handleCancel,
-  handleSubmit
-}) => {
+const AddCourseContainer = () => {
   const [pageInfo, setPageInfo] = useState({
     file: null,
     fileName: "",
+    fileTwo: null,
+    fileTwoName: "",
     shortInputOne: "",
     shortInputTwo: "",
     freeTypeInputOne: "",
@@ -30,14 +18,26 @@ const AddCourseContainer = ({
   const [pageFirstOpened, setPageFirstOpened] = useState(true);
 
   const handleFileChange = (e) => {
-    if (!pageInfo.file) setPageInfo({ fileName: e.target.files[0].name });
-    pageInfo.file
-      ? setPageInfo({ ...pageInfo, file: null })
-      : setPageInfo({
-          ...pageInfo,
-          file: URL.createObjectURL(e.target.files[0]),
-        });
-  };
+    console.log(pageInfo)
+      if (!pageInfo.file) setPageInfo({ fileName: e.target.files[0].name });
+      pageInfo.file
+        ? setPageInfo({ ...pageInfo, file: null })
+        : setPageInfo({
+            ...pageInfo,
+            file: URL.createObjectURL(e.target.files[0]),
+          });
+    }
+
+  const handleFileChange2 = (e) => {
+    console.log(pageInfo)
+    if (!pageInfo.fileTwo) setPageInfo({ fileTwoName: e.target.files[0].name });
+      pageInfo.fileTwo
+        ? setPageInfo({ ...pageInfo, fileTwo: null })
+        : setPageInfo({
+            ...pageInfo,
+            fileTwo: URL.createObjectURL(e.target.files[0]),
+          });
+  }
 
   const handleChange = (e) => {
     console.log(e.target.name);
@@ -47,110 +47,63 @@ const AddCourseContainer = ({
   const storeInputs = (event) => {
     setPageFirstOpened(false);
     event.preventDefault();
-    if (
-      (pageInfo.shortInputOne == "" && !shortLabelTextOne.includes("optional")) ||
-      (pageInfo.freeTypeInputOne == "" && !freeTypeLabelTextOne.includes("optional")) ||
-      (pageInfo.shortInputTwo == "" && !shortLabelTextTwo.includes("optional"))
-    ) {
+    if (pageInfo.shortInputOne == "" || pageInfo.freeTypeInputOne == "" || pageInfo.shortInputTwo == "") {
       setErrorMessage("Please enter the info needed in the empty red boxes");
     } else {
       setErrorMessage("");
     }
   };
+
+  
+  const classNameInputTwo = (pageInfo.shortInputTwo != "" && !pageFirstOpened) ||
+  (pageFirstOpened) 
+  ? "short__input"
+  : "short__input--empty"
+  
+  
+  const classNameInput = (pageInfo.shortInputOne != "" && !pageFirstOpened) ||
+  (pageFirstOpened) 
+  ? "short__input"
+  : "short__input--empty"
+  
+  const classNameInputThree = (pageInfo.freeTypeInputOne != ""  && !pageFirstOpened) ||
+  (pageFirstOpened)
+    ? "free-type__input"
+    : "free-type__input--empty"
+
+  const file= pageInfo.file
+
+  const fileName= pageInfo.fileName
+
+  const fileTwo = pageInfo.fileTwo
+
+  const fileNameTwo = pageInfo.fileName
+  
   return (
     <div className="add-course">
-      <div className="add-course__input-texts">
-        <Short
-          shortLabelText={shortLabelTextOne}
-          shortType="text"
-          name="shortInputOne"
-          inputClassName={
-            (pageInfo.shortInputOne != "" && !shortLabelTextOne.includes("optional") && !pageFirstOpened) ||
-            (pageFirstOpened && !shortLabelTextOne.includes("optional")) ||
-            shortLabelTextOne.includes("optional")
-              ? "short__input"
-              : "short__input--empty"
-          }
-          handleShortValue={handleChange}
-        />
-        <FreeType
-          freeTypeLabelText={freeTypeLabelTextOne}
-          name="freeTypeInputOne"
-          inputClassName={
-            (pageInfo.freeTypeInputOne != "" && !freeTypeLabelTextOne.includes("optional") && !pageFirstOpened) ||
-            (pageFirstOpened && !freeTypeLabelTextOne.includes("optional")) ||
-            freeTypeLabelTextOne.includes("optional")
-              ? "free-type__input"
-              : "free-type__input--empty"
-          }
-          handleFreeTypeValue={handleChange}
-        />
-        <Short
-          shortLabelText={shortLabelTextTwo}
-          shortType="text"
-          shortPlaceHolderText={shortPlaceHolderTextTwo}
-          name="shortInputTwo"
-          inputClassName={
-            (pageInfo.shortInputTwo != "" && !shortLabelTextTwo.includes("optional") && !pageFirstOpened) ||
-            (pageFirstOpened && !shortLabelTextTwo.includes("optional")) ||
-            shortLabelTextTwo.includes("optional")
-              ? "short__input"
-              : "short__input--empty"
-          }
-          handleShortValue={handleChange}
-        />
-        <Short
-          shortLabelText={shortLabelTextThree}
-          shortType="text"
-          shortPlaceHolderText={shortPlaceHolderTextThree}
-          name="shortInputTwo"
-          inputClassName={
-            (pageInfo.shortInputTwo != "" && !shortLabelTextTwo.includes("optional") && !pageFirstOpened) ||
-            (pageFirstOpened && !shortLabelTextTwo.includes("optional")) ||
-            shortLabelTextTwo.includes("optional")
-              ? "short__input"
-              : "short__input--empty"
-          }
-          handleShortValue={handleChange}
-        />
-
-        <p className="add-course__error-message">{errorMessage}</p>
-
-        <div className="add-course__buttons">
-          <div className="add-course__cancel">
-            <Button style={"button light-grey round-border large"} textStyle={"text large-text light-grey"} buttonText={"Cancel"} onClick={handleCancel} />
-          </div>
-          <div className="add-course__submit">
-            <Button
-              style={"button black round-border large"}
-              textStyle={"text large-text black"}
-              buttonText={"Submit"}
-              buttonFunction={storeInputs}
-              onClick={handleSubmit}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="add-course__uploadContainer">
-        <div className="add-course__media-upload">
-          <MediaUploadBox
-            file={pageInfo.file}
-            handleFileChange={handleFileChange}
-            uploadLabelName={uploadLabelName}
-            uploadButtonText="Upload Image"
-            fileName={pageInfo.fileName}
-          />
-        </div>
-        <div className="add-course__media-upload2">
-          <MediaUploadBox
-            file={pageInfo.file}
-            handleFileChange={handleFileChange}
-            uploadLabelName={uploadLabelNameTwo}
-            uploadButtonText="Upload Image"
-            fileName={pageInfo.fileName}
-          />
-        </div>
-      </div>
+      <AddCourse
+        shortLabelTextOne={"Course Name"}
+        freeTypeLabelTextOne={"Course Overview"}
+        shortLabelTextTwo={"Course Provider"}
+        shortPlaceHolderTextTwo={"E.G Barclays..."}
+        shortLabelTextThree={"Course Mentor"}
+        shortPlaceHolderTextThree={"E.G Barclays"}
+        uploadLabelName={"Course Thumbnail"}
+        uploadLabelNameTwo={"Provider Logo"}
+        classNameInput={classNameInput}
+        classNameInputTwo={classNameInputTwo}
+        classNameInputThree={classNameInputThree}
+        handleChange={handleChange}
+        handleFileChange={handleFileChange}
+        handleFileChange2={handleFileChange2}
+        storeInputs={storeInputs}
+        errorMessage={errorMessage}
+        pageFirstOpened={pageFirstOpened}
+        file={file}
+        fileName={fileName}
+        fileTwo={fileTwo}
+        fileNameTwo={fileNameTwo}
+      />
     </div>
   );
 };
