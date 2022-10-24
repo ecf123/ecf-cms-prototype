213e038d8bf434813cdb0aceea6b2db72beca32e
Amelia Baldwin <amelia01000010@gmail.com>
2022-10-24 16:47:22 +0100

    added test, formatted

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,6 @@
 import Layout from "../../components/Layout/Layout";
-import profile from "../../assets/profile-picture.svg";
-const Home = () => {
 
-const user = {
-  image: profile,
-  name: "Brett Zieme",
-  role: "Admin",
-};
-  return <Layout user={user}>
-  </Layout>;
+const Home = () => {
+  return <Layout></Layout>;
 };
 export default Home;
