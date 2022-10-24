c4b85f8a265f2a19bd35dcdabfc3f112806460ef
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-10-24 16:08:45 +0100

    corrected login tests

--- src/components/Login/Login.test.jsx
+++ src/components/Login/Login.test.jsx
@@ -1,6 +1,5 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Login from "./Login";
-import userEvent from "@testing-library/user-event";
 import { customRender } from "../../utils/testUtils";
 
 it("should render the form", () => {
