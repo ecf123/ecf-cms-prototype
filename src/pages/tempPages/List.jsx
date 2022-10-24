1bc26580fba68ab96fe7e216699b2441346cec49
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-24 13:55:23 +0100

    changed temp files to use list containers

--- src/pages/tempPages/List.jsx
+++ src/pages/tempPages/List.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CourseListContainer from "../../containers/CourseListContainer/CourseListContainer";
 
 const List = () => {
+
+    const dataJSON = [
+        {
+          id: "kjsvswkvfsihvds",
+          challenges: [
+            {
+              duration: "15 minutes",
+              type: "assignment",
+            },
+            {
+              duration: "1 hrs",
+              type: "assignment",
+            },
+          ],
+          lessons: [
+            {
+              duration: "15 minutes",
+              type: "Video",
+            },
+            {
+              duration: "10 minutes",
+              type: "Video",
+            },
+            {
+              duration: "15 minutes",
+              type: "Video",
+            },
+          ],
+          title: "Halifax",
+          name: "Halifax as provider",
+        },
+        {
+          id: "fsdfsdfdghsfghs",
+          challenges: [
+            {
+              duration: "30 minutes",
+              type: "assignment",
+            },
+            {
+              duration: "2 hrs 20 minutes",
+              type: "assignment",
+            },
+          ],
+          lessons: [
+            {
+              duration: "25 minutes",
+              type: "Video",
+            },
+            {
+              duration: "10 minutes",
+              type: "Video",
+            },
+            {
+              duration: "15 minutes",
+              type: "Video",
+            },
+          ],
+          title: "Barclays",
+          name: "Barclays as provider",
+        },
+      ];
     return (
-        <Link to="/course" >Single Course</Link>
+        <>
+            <Link to="/course" >Single Course</Link>
+            <CourseListContainer dataJSON={dataJSON} />
+        </>
     )
 }
 
