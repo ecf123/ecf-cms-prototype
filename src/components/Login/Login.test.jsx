288330ee8ab673a33b71159295cbb2cdfdcd9418
Andy Bowerman <andy_bowerman06@hotmail.com>
2022-10-24 16:05:28 +0100

    Created registration form with styling and testing

--- src/components/Login/Login.test.jsx
+++ src/components/Login/Login.test.jsx
@@ -22,39 +22,3 @@ it("shouldn't render the success message on load", () => {
   );
   expect(successMessage).toBeFalsy();
 });
-
-it("should not submit the form when not all credentials are valid and display error message", () => {
-  render(<Login />);
-
-  const passwordInput = screen.getByLabelText("Password");
-  userEvent.type(passwordInput, "fix");
-
-  const button = screen.queryByText("Login");
-  userEvent.click(button);
-
-  const errorMessage = screen.getByText("Sorry something went wrong");
-  const successMessage = screen.queryByText(
-    "Thank you for submitting! We will be in touch"
-  );
-
-  expect(errorMessage).toBeTruthy();
-  expect(successMessage).toBeFalsy();
-});
-
-it("should submit the form when all credentials are valid and display success message", () => {
-  render(<Login />);
-
-  const emailInput = screen.getByLabelText("Email Address");
-  userEvent.type(emailInput, "lucy@gmail.com");
-
-  const passwordInput = screen.getByLabelText("Password");
-  userEvent.type(passwordInput, "nology2022");
-
-  fireEvent.submit(screen.getByTestId("form"));
-
-  const successMessage = screen.getByTestId("success");
-  const errorMessage = screen.queryByText("Sorry something went wrong");
-
-  expect(successMessage).toBeInTheDocument();
-  expect(errorMessage).toBeFalsy();
-});
