38b130cb63c3356c2d8fe2e5b91149c383a90236
atif298 <atifuddin03@gmail.com>
2022-10-25 10:14:07 +0100

    AddArticle Page designed

--- src/pages/AddArticle/AddArticle.jsx
+++ src/pages/AddArticle/AddArticle.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import Layout from "../../components/Layout/Layout.jsx";
 import AddPage from "../../containers/AddPage/AddPage";
 
 const AddArticle = () => {
   return (
-    <>
-        <AddPage/>
-    </>
+    <Layout>
+        <AddPage 
+            shortLabelTextOne="Article Name"
+            shortLabelTextTwo="Additional Subtitle(s) (optional)"
+            shortPlaceHolderTextOne="e.g. Financial Forest"
+            shortPlaceHolderTextTwo="e.g. Fund Manager"
+            freeTypeLabelTextOne="Article Overview"
+            freeTypeLabelTextTwo="Additional Content (optional)"
+            uploadLabelName="Article Thumbnail"
+            />
+    </Layout>
   )
 }
 
-export default AddArticle
+export default AddArticle;
