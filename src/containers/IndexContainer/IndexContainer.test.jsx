88d4212537e02304ac77b5e58b68c91ac51ef5d6
Connel Nwaodor <connel_n@hotmail.com>
2022-10-25 15:47:21 +0100

    done

--- src/containers/IndexContainer/IndexContainer.test.jsx
+++
@@ -1,9 +0,0 @@
-import { customRender } from "../../utils/testUtils";
-import IndexContainer from "./IndexContainer";
-
-it("Should match the snapshot", () => {
-    const { container } = customRender(
-      <IndexContainer articleData={"articleData"} optionsData={"OptionsData"} handleEdit={"handleEdit"} handleDelete={"handleDelete"} />
-    );
-    expect(container).toMatchSnapshot();
-  });
