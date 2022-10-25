4ff3fb944cf3cc7f698dee76a658abe4b469b8d2
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-10-25 12:16:03 +0100

    added single pathway page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./styles/main.scss";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import SinglePathway from "./pages/SinglePathway/SinglePathway";
 
 const App = () => {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
+      <Route path="pathway" element={<SinglePathway/>} />
     </Routes>
   );
 };
