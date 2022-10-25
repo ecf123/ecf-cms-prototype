ca060a4e027b2bc491aa56c476789792a5aaf922
Gar-Yin Lam <garyin.lam@gmail.com>
2022-10-25 09:35:42 +0100

    building out temp files

--- src/pages/tempPages/ArticleList.jsx
+++ src/pages/tempPages/ArticleList.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ListContainer from '../../containers/ListContainer/ListContainer'
 
 const ArticleList = () => {
+    const data = [
+        {
+          articleContent: [
+            {
+              heading: "Heading 1",
+              paragraph:
+                "Lorem ipsum dolor sit amet. Id earum enim eum maxime nihil eos commodi quisquam non amet magnam eum sunt voluptatem et laborum magni. Et odio quas aut quam modi ea beatae officia eos quidem internos! Est quidem maiores in itaque earum eos incidunt voluptatibus cum obcaecati consequuntur ea accusantium animi est voluptatem omnis aut voluptatem distinctio. Qui corrupti voluptatem ea asperiores quod non fugiat nihil aut iste pariatur qui porro natus? Ut velit doloribus ut officia impedit repellendus reprehenderit in nisi asperiores. Est doloremque error ab esse enim et nesciunt quasi qui ipsa atque. In illo fugit qui provident cupiditate ut molestias odio in repudiandae eligendi est commodi quia. Qui dolorem iste eum voluptatum sint non error doloribus? Ut totam velit rem voluptatem amet qui natus doloribus id numquam odio sed similique vitae et quod officia. Hic repellat magni et fuga pariatur est itaque sint.",
+            },
+            {
+              heading: "Heading 1",
+              paragraph:
+                "Lorem ipsum dolor sit amet. Id earum enim eum maxime nihil eos commodi quisquam non amet magnam eum sunt voluptatem et laborum magni. Et odio quas aut quam modi ea beatae officia eos quidem internos! Est quidem maiores in itaque earum eos incidunt voluptatibus cum obcaecati consequuntur ea accusantium animi est voluptatem omnis aut voluptatem distinctio. Qui corrupti voluptatem ea asperiores quod non fugiat nihil aut iste pariatur qui porro natus? Ut velit doloribus ut officia impedit repellendus reprehenderit in nisi asperiores. Est doloremque error ab esse enim et nesciunt quasi qui ipsa atque. In illo fugit qui provident cupiditate ut molestias odio in repudiandae eligendi est commodi quia. Qui dolorem iste eum voluptatum sint non error doloribus? Ut totam velit rem voluptatem amet qui natus doloribus id numquam odio sed similique vitae et quod officia. Hic repellat magni et fuga pariatur est itaque sint.",
+            },
+          ],
+          category: "Finance",
+          date: "23 May 2022",
+          id: 2,
+          isActive: false,
+          readTime: "5 mins",
+          thumbnail:
+            "https://firebasestorage.googleapis.com/v0/b/ecf-future-hub.appspot.com/o/cv-pic.jpg?alt=media&token=8f649302-9820-426d-80f2-2337e970fc71",
+          title: "Test title one",
+        },
+        {
+          articleContent: [
+            {
+              heading: "Heading 1",
+              paragraph:
+                "Lorem ipsum dolor sit amet. Id earum enim eum maxime nihil eos commodi quisquam non amet magnam eum sunt voluptatem et laborum magni. Et odio quas aut quam modi ea beatae officia eos quidem internos! Est quidem maiores in itaque earum eos incidunt voluptatibus cum obcaecati consequuntur ea accusantium animi est voluptatem omnis aut voluptatem distinctio. Qui corrupti voluptatem ea asperiores quod non fugiat nihil aut iste pariatur qui porro natus? Ut velit doloribus ut officia impedit repellendus reprehenderit in nisi asperiores. Est doloremque error ab esse enim et nesciunt quasi qui ipsa atque. In illo fugit qui provident cupiditate ut molestias odio in repudiandae eligendi est commodi quia. Qui dolorem iste eum voluptatum sint non error doloribus? Ut totam velit rem voluptatem amet qui natus doloribus id numquam odio sed similique vitae et quod officia. Hic repellat magni et fuga pariatur est itaque sint.",
+            },
+            {
+              heading: "Heading 1",
+              paragraph:
+                "Lorem ipsum dolor sit amet. Id earum enim eum maxime nihil eos commodi quisquam non amet magnam eum sunt voluptatem et laborum magni. Et odio quas aut quam modi ea beatae officia eos quidem internos! Est quidem maiores in itaque earum eos incidunt voluptatibus cum obcaecati consequuntur ea accusantium animi est voluptatem omnis aut voluptatem distinctio. Qui corrupti voluptatem ea asperiores quod non fugiat nihil aut iste pariatur qui porro natus? Ut velit doloribus ut officia impedit repellendus reprehenderit in nisi asperiores. Est doloremque error ab esse enim et nesciunt quasi qui ipsa atque. In illo fugit qui provident cupiditate ut molestias odio in repudiandae eligendi est commodi quia. Qui dolorem iste eum voluptatum sint non error doloribus? Ut totam velit rem voluptatem amet qui natus doloribus id numquam odio sed similique vitae et quod officia. Hic repellat magni et fuga pariatur est itaque sint.",
+            },
+          ],
+          category: "Finance",
+          date: "23 May 2022",
+          id: 2,
+          isActive: false,
+          readTime: "5 mins",
+          thumbnail:
+            "https://firebasestorage.googleapis.com/v0/b/ecf-future-hub.appspot.com/o/cv-pic.jpg?alt=media&token=8f649302-9820-426d-80f2-2337e970fc71",
+          title: "Test title two",
+        },
+      ];
+
     return (
         <div>
+            <ListContainer title="Article" data={data} />
             <p>ArticleList</p>
             <Link to="/addarticle" >Add article  </Link>
             <Link to="singlearticle" >Article title</Link>
--- src/pages/tempPages/Marketplace.jsx
+++ src/pages/tempPages/Marketplace.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ListContainer from '../../containers/ListContainer/ListContainer'
 
 const Marketplace = () => {
+
+    const data = [
+        {
+          heading: "Earn a £5 Amazon Voucher",
+          id: 1,
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/ecf-future-hub.appspot.com/o/market-place%2Famazon-logo.png?alt=media&token=0c9914b9-b020-421c-acaa-e87ff25be8a0",
+          isLocked: false,
+          overview:
+            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic placeat quis blanditiis. Debitis placeat sunt, sapiente, a iste repudiandae hic necessitatibus natus inventore maxime, perspiciatis sed distinctio praesentium blanditiis ipsa? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic placeat quis blanditiis. Debitis placeat sunt, sapiente, a iste repudiandae hic necessitatibus natus inventore maxime, perspiciatis sed distinctio praesentium blanditiis ipsa? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic placeat quis blanditiis. Debitis placeat sunt, sapiente, a iste repudiandae hic necessitatibus natus inventore maxime, perspiciatis sed distinctio praesentium blanditiis ipsa?",
+          skillPoints: 400,
+          trophies: 1,
+        },
+      ];
+
     return (
-        <div>Marketplace<br/>
+        <div>
+            <ListContainer title="Marketplace" data={data} />
+            
+            Marketplace<br/>
             <Link to="/addprize">Add prize</Link><br/>
             <Link to="singleitem" >SingleItem</Link>
         </div>
