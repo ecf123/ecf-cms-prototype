cb860fb17078ced1b93e90e2f5b64e39bf96ba88
michal <michalwagh@gmail.com>
2022-10-25 10:20:32 +0100

    login page complited

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
-import Layout from "../../components/Layout/Layout";
+// import Layout from "../../components/Layout/Layout";
+import LoginPage from "../LogInPage/LoginPage";
 
 const Home = () => {
-  return <Layout></Layout>;
+  return <LoginPage />
 };
 export default Home;
