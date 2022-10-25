dcff8e0ffecd819b4dcf948ac561c9274a44d899
AdrianRotila <adrianrotila@gmail.com>
2022-10-25 12:53:13 +0100

    code refactor

--- src/containers/PrizeContainer/PrizeContainer.jsx
+++ src/containers/PrizeContainer/PrizeContainer.jsx
@@ -1,29 +1,43 @@
-import "./PrizeContainer.scss"
-import MoreOptions from "../../components/MoreOptions/MoreOptions"
-import Card from "../../components/Card/Card"
+import "./PrizeContainer.scss";
+import MoreOptions from "../../components/MoreOptions/MoreOptions";
+import Card from "../../components/Card/Card";
 
-const PrizeContainer = ({prizeData, optionsData}) => {
-  
-const prizeHeading = prizeData.heading;
-const prizeImage = prizeData.image;
-const prizeOverview = prizeData.overview;
-const prizeSkills = prizeData.skillPoints;
-const prizeDate = prizeData.date;
-const prizeCondition = prizeData.isLocked;
-const optionTitle = optionsData.title;
-const optionDesc = optionsData.desc;
-
+const PrizeContainer = ({
+  prizeData,
+  optionsData,
+  handleEdit,
+  handleDelete,
+}) => {
+  const prizeHeading = prizeData.heading;
+  const prizeImage = prizeData.image;
+  const prizeOverview = prizeData.overview;
+  const prizeSkills = prizeData.skillPoints;
+  const prizeDate = prizeData.date;
+  const prizeCondition = prizeData.isLocked;
+  const optionTitle = optionsData.title;
+  const optionDesc = optionsData.desc;
 
   return (
     <div className="prize-container">
-        <div className="prize-container__left-column"/>
-        <div className="prize-container__middle-column">
-          <Card title={prizeHeading} img={prizeImage} dateOrTime={prizeDate} cardInfo={prizeOverview} links={prizeSkills} condition={prizeCondition}/>
-        </div>
-        <div className="prize-container__right-column">
-          <MoreOptions title={optionTitle} description={optionDesc} />
-        </div>
+      <div className="prize-container__middle-column">
+        <Card
+          title={prizeHeading}
+          img={prizeImage}
+          dateOrTime={prizeDate}
+          cardInfo={prizeOverview}
+          links={prizeSkills}
+          condition={prizeCondition}
+        />
+      </div>
+      <div className="prize-container__right-column">
+        <MoreOptions
+          title={optionTitle}
+          description={optionDesc}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+        />
+      </div>
     </div>
-  )
-}
-export default PrizeContainer
+  );
+};
+export default PrizeContainer;
