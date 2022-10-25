a594a9fe3356b5f6d3d552dbe2e9804aa861b3d0
atif298 <atifuddin03@gmail.com>
2022-10-25 09:47:41 +0100

    Testing render

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./styles/main.scss";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
+// import Home from "./pages/Home/Home";
+import AddArticle from "./pages/AddArticle/AddArticle";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="*" element={<Home />} />
+      <Route path="*" element={<AddArticle />} />
     </Routes>
   );
 };
---
+++ src/pages/AddArticle/AddArticle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddPage from "../../containers/AddPage/AddPage";
+
+const AddArticle = () => {
+  return (
+    <>
+        <AddPage/>
+    </>
+  )
+}
+
+export default AddArticle
